e952bfe739178b2b6833de8edbe3c4e768a4f74c
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-03-08 19:31:34 -0800

    Clean up model_observer.py

--- analysis/model_observer.py
+++ analysis/model_observer.py
@@ -1,26 +1,21 @@
 import numpy as np
-from scipy.integrate import trapz
 
-import cogphysics.lib.rvs as rvs
-import cogphysics.lib.nplib as npl
-import cogphysics.lib.circ as circ
+from stat_util import normalize
 
-import pdb
+# from joblib import Memory
+# memory = Memory(cachedir="cache", mmap_mode='c', verbose=0)
 
-from joblib import Memory
-memory = Memory(cachedir="cache", mmap_mode='c', verbose=0)
-
-normalize = rvs.util.normalize
-weightedSample = rvs.util.weightedSample
 
 def make_kde_smoother(x, lam):
     dists = np.abs(x[:, None] - x[None, :]) / lam
     pdist = np.exp(-0.5 * dists**2) / np.sqrt(2)
     sum_pdist = np.sum(pdist, axis=-1)
+
     def kde_smoother(y):
         est = np.sum(pdist * y, axis=-1) / sum_pdist
         return est
     return kde_smoother
+
 
 def IPE(F, samps, kappas, smooth):
     """Evaluate the posterior probability of the outcome given
@@ -31,7 +26,7 @@ def IPE(F, samps, kappas, smooth):
     Parameters
     ----------
     F : array-like (..., n_trial)
-        outcomes 
+        outcomes
     samps : array-like (n_trial, n_kappas, n_samples)
         samples from the IPE
     smooth : boolean
@@ -45,15 +40,15 @@ def IPE(F, samps, kappas, smooth):
 
     n_trial, n_kappas, n_samps = samps.shape
     x = np.array(kappas)
-    
+
     # shape is (n_trial, n_kappas)
     alpha = np.sum(samps, axis=-1) + 0.5
     beta = np.sum(1-samps, axis=-1) + 0.5
     pfell_mean = alpha / (alpha + beta)
-    pfell_var = (alpha*beta) / ((alpha+beta)**2 * (alpha+beta+1))
-    pfell_std = np.sqrt(pfell_var)
+    # pfell_var = (alpha*beta) / ((alpha+beta)**2 * (alpha+beta+1))
+    # pfell_std = np.sqrt(pfell_var)
     # shape is (n_trial,)
-    pfell_meanstd = np.mean(pfell_std, axis=-2)
+    # pfell_meanstd = np.mean(pfell_std, axis=-2)
 
     if not smooth:
         # using bernoulli fall/not fall
@@ -77,7 +72,7 @@ def IPE(F, samps, kappas, smooth):
     # Ok, so now we have pfell which is shape (n_trial, n_kappas) and
     # is the probability of the stimulus on trial N falling, given
     # kappa. So now we need to actually evaluate that probability...
-        
+
     # likelihood of falling = pfell
     lh1 = F[..., None] * pfell
     # likelihood of not falling = 1-pfell
@@ -88,7 +83,8 @@ def IPE(F, samps, kappas, smooth):
     #pdf = np.swapaxes((lh1 + lh0)[..., 0, 0], -2, -1)
     return lh
 
-def predict(p_kappas, ipe_samps, kappas, smooth): 
+
+def predict(p_kappas, ipe_samps, kappas, smooth):
     """Predict the likelihood of the outcome given the stimulus, P(F_t
     | S_t), independent of kappa.
 
@@ -104,7 +100,7 @@ def predict(p_kappas, ipe_samps, kappas, smooth):
     """
 
     n_trial, n_kappas, n_samples = ipe_samps.shape
-    
+
     # get the log likelihood, P(F | S, k), in the shape of (n_trial,
     # n_kappas)
     llh = np.log(IPE(np.ones(n_trial), ipe_samps, kappas, smooth))
@@ -116,7 +112,9 @@ def predict(p_kappas, ipe_samps, kappas, smooth):
     p_outcomes = normalize(joint, axis=-1)[0]
     return p_outcomes
 
-def ModelObserver(feedback, ipe_samps, kappas, prior=None, p_ignore=0.0, smooth=True):
+
+def ModelObserver(feedback, ipe_samps, kappas,
+                  prior=None, p_ignore=0.0, smooth=True):
     """Computes a learning curve for a model observer, given raw
     samples from their internal 'intuitive mechanics engine', the
     prior over mass ratios, and the probability of each possible
@@ -148,7 +146,7 @@ def ModelObserver(feedback, ipe_samps, kappas, prior=None, p_ignore=0.0, smooth=
     # prior, if none is given, shape of (n_kappas,)
     if prior is None:
         prior = np.log(np.ones(n_kappas) / n_kappas)
-    
+
     # get the log likelihood, P(F | S, k), in the shape of (...,
     # n_trial, n_kappas)
     lh = np.log(IPE(feedback, ipe_samps, kappas, smooth))
@@ -165,9 +163,13 @@ def ModelObserver(feedback, ipe_samps, kappas, prior=None, p_ignore=0.0, smooth=
 
     return joint, posterior
 
-def EvaluateObserver(responses, feedback, ipe_samps, kappas, prior=None, p_ignore=0.0, smooth=True):
+
+def EvaluateObserver(responses, feedback, ipe_samps, kappas,
+                     prior=None, p_ignore=0.0, smooth=True):
+
     joint, posterior = ModelObserver(
-        feedback, ipe_samps, kappas, prior=prior, p_ignore=p_ignore, smooth=smooth)
+        feedback, ipe_samps, kappas,
+        prior=prior, p_ignore=p_ignore, smooth=smooth)
 
     n_trial, n_kappas, n_samps = ipe_samps.shape
     lh = np.log(IPE(np.ones(n_trial), ipe_samps, kappas, smooth))
@@ -175,17 +177,20 @@ def EvaluateObserver(responses, feedback, ipe_samps, kappas, prior=None, p_ignor
 
     p_fall = np.exp(normalize(posterior[..., :-1, :] + lh, axis=-1)[0])
     p_response = (p_fall * (1-p_ignore)) + ((1-p_fall) * (p_ignore))
-    
+
     # marginal and responses should be in the shape (..., n_trial)
     lh0 = responses[:, None] * p_response
     lh1 = (1-responses)[:, None] * (1-p_response)
     lh = lh0 + lh1
     lh[np.isnan(lh)] = 0.5
-    assert (lh<=1).all()
+    assert (lh <= 1).all()
     llh = np.sum(np.log(lh), axis=-1)
     return llh
 
-def simulateResponses(n, feedback, ipe_samps, kappas, prior=None, p_ignore=0.0, smooth=True):
+
+def simulateResponses(n, feedback, ipe_samps, kappas,
+                      prior=None, p_ignore=0.0, smooth=True):
+
     # learning model beliefs
     model_joint, model_theta = ModelObserver(
         feedback, ipe_samps, kappas, prior=prior, smooth=smooth)
