6bc091f13e8a375fda2fd2c1df49aa5c94342b58
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-03-11 01:11:19 -0700

    Clean up tower scene

--- scripts/render/tower_scene.py
+++ scripts/render/tower_scene.py
@@ -1,48 +1,27 @@
-import cogphysics
-import cogphysics.lib.funclib as fl
-import cogphysics.lib.geometry as geom
-import cogphysics.lib.physutil as physutil
-
 from cogphysics.core import cpObject
-from cogphysics.core.graphics import PandaGraphics as Graphics
 from cogphysics.core.physics import OdePhysics as Physics
 
-import panda3d.core as p3d
-import pandac.PandaModules as pm
-import libpanda as lp
-from direct.gui.OnscreenText import OnscreenText, TextNode
-from direct.showbase.ShowBase import ShowBase
-
-import datetime
-import numpy as np
-import os
-import pdb
-import random
-import sys
 
 class TowerScene(object):
 
-    def __init__(self):
+    def __init__(self, cpopath):
         self.scene = None
         self.tower = None
         self.blocks = []
         self.table = None
 
-        self.cpopath = cogphysics.path(os.path.join(
-            cogphysics.CPOBJ_PATH, "mass", "new-stability-towers"), 'local', 'afs')
-        # self.cpopath = cogphysics.path(os.path.join(
-        #     cogphysics.CPOBJ_PATH, "mass", "towers"), 'local')
+        self.cpopath = cpopath
 
     @classmethod
     def create(cls, stimname, table=None, cpopath=None):
 
         # create the object to hold the scene
-        obj = cls()
+        obj = cls(cpopath)
 
         # update the cpopath, if necessary
         if cpopath:
             obj.cpopath = cpopath
-        
+
         # create an empty scene
         obj.scene = cpObject()
 
@@ -82,7 +61,7 @@ class TowerScene(object):
     @staticmethod
     def makeTable():
         table = cpObject(
-            label='table', 
+            label='table',
             surface='old_cpo_surface',
             model='wood_floor',
             scale=(10, 10, 1),
@@ -91,7 +70,7 @@ class TowerScene(object):
             density=None,
             shape='cylinderZ',
             color=(0.45, 0.3, 0.1, 1.0))
-            
+
         return table
 
     def setBlockProperties(self):
@@ -115,15 +94,9 @@ class TowerScene(object):
         for block in self.blocks:
             block.destroy()
         self.blocks = []
-        #if self.table:
-        #    self.table.destroy()
-        #    self.table = None
         if self.tower:
             self.tower.destroy()
             self.tower = None
         if self.scene:
             self.scene.destroy()
             self.scene = None
-
-    # def __del__(self):
-    #     self.destroy()
