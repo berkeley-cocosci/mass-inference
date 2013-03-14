ba549b1e6a49d870aa2c1f32824ce722f08cb498
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-03-13 23:07:40 -0700

    Only record feedback for r=10 for F stimuli

--- scripts/render/record_playback_F.sh
+++ scripts/render/record_playback_F.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-python record_playback.py --stype=mass-oneshot-example-F mass-oneshot-example-F~kappa--1.0 mass-oneshot-example-F~kappa-1.0
-python record_playback.py --stype=mass-oneshot-F mass-oneshot-F~kappa--1.0 mass-oneshot-F~kappa-1.0
+python record_playback.py --stype=mass-oneshot-example-F mass-oneshot-example-F~kappa-1.0
+python record_playback.py --stype=mass-oneshot-F mass-oneshot-F~kappa-1.0
 python record_playback.py --stype=mass-oneshot-training-F --original
 python record_playback.py --stype=stability-example-stable-F --original
 python record_playback.py --stype=stability-example-unstable-F --original
