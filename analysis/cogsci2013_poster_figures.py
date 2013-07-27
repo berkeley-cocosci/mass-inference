f00c2e37eade0f7f44db0d7ef65ad2d27a448816
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-07-27 14:29:37 -0700

    Bootstrap analysis of model likelihoods and figure tweaks
    Fix bugs in analyze-stability-learning.ipynb

--- analysis/cogsci2013_poster_figures.py
+++ analysis/cogsci2013_poster_figures.py
@@ -4,6 +4,7 @@ import numpy as np
 from snippets import graphing
 import itertools
 import matplotlib.gridspec as gridspec
+from stats_tools import normalize
 
 
 def join(*args):
@@ -56,9 +57,13 @@ def _wcih_trial(ax, trial, ridx, all_trials, wcih, opts):
         if kwargs['hatch']:
             ax.bar(bar_x[i], mean[idx], color='none', **basekwargs)
 
-        if not sig[idx]:
-            ax.text(bar_x[i], mean[idx]+0.01, 'ns',
-                    horizontalalignment='center')
+        if sig[idx] < 0.05:
+            ptext = "*"
+        else:
+            ptext = ""
+
+        ax.text(bar_x[i], mean[idx]+0.01, ptext,
+                horizontalalignment='center')
 
     # set axis limits
     ax.set_ylim(*ylim)
@@ -112,6 +117,8 @@ def _wcih_all(ax, ridx, oidx, all_trials, wcih, opts):
     xlim = 1, 40
     ylim = 0.3, 1.01
     xticks = all_trials
+    xticklabels = [str(x) for x in xticks]
+    xticklabels[1] = ""
 
     ax.plot(xlim, [0.5, 0.5], 'k:', linewidth=3)
 
@@ -122,8 +129,7 @@ def _wcih_all(ax, ridx, oidx, all_trials, wcih, opts):
     # set axis tick locations and labels
     ax.set_yticks([])
     ax.set_xticks(xticks)
-    ax.xaxis.tick_top()
-    ax.set_xticklabels([])
+    ax.set_xticklabels(xticklabels)
 
     # disable tick marks
     ax.xaxis.set_ticks_position('both')
@@ -186,24 +192,30 @@ def plot_wcih(all_trials, wcih, opts):
             if tidx == 1:
                 graphing.clear_left(ax)
 
-            if ridx == 1:
-                xticklabels = [
-                    "%s" % labels.get(join(order)).capitalize()
-                    for order in orders]
-                ax.set_xticklabels(xticklabels, fontsize=20)
+            xticklabels = [
+                "%s" % labels.get(join(order)).capitalize()
+                for order in orders]
+            ax.set_xticklabels(xticklabels, fontsize=20)
+
+            for tick in ax.xaxis.get_ticklabels():
+                pos = list(tick.get_position())
+                pos[1] = -0.10
+                tick.set_position(pos)
 
         else:
             _wcih_all(ax, ridx, oidx, all_trials, wcih, opts)
 
             if ridx == 0:
                 ax.set_title("All Trials", fontsize=28, position=(0.5, 1.15))
-                xticklabels = map(str, map(int, all_trials))
-                ax.set_xticklabels(xticklabels, fontsize=16)
-            # if oidx == 0:
-            #     ax.set_yticks(np.arange(0.3, 1.1, 0.1))
-            #     ax.set_yticklabels(["%d%%" % x for x in xrange(30, 110, 10)])
-            if ridx == 1:
-                ax.set_xlabel(labels[order].capitalize())
+
+            ax.set_xlabel("%s, $r_0=%s$" % (
+                labels[order].capitalize(),
+                ratio))
+            ax.xaxis.set_label_coords(0.5, -0.125)
+
+            if oidx == 0:
+                ax.set_yticks(np.arange(0.3, 1.1, 0.1))
+                ax.set_yticklabels(["%d%%" % x for x in xrange(30, 110, 10)])
 
     ax = axes[0, 1]
     legend_lines = [
@@ -214,19 +226,27 @@ def plot_wcih(all_trials, wcih, opts):
     ax.legend(legend_lines,
               ['Binary f.b.', 'Visual f.b.', 'Learning IPE'],
               loc='upper left', frameon=False, ncol=3,
-              bbox_to_anchor=(-1.05, 1.183), fontsize=16)
+              bbox_to_anchor=(-1.175, 1.09), fontsize=16)
 
     fig.set_figwidth(16)
-    fig.set_figheight(7.5)
+    fig.set_figheight(7.1)
 
-    plt.subplots_adjust(left=0.09, bottom=0.05, top=0.875,
-                        right=0.99, wspace=0.1, hspace=0.2)
+    plt.subplots_adjust(left=0.09, bottom=0.1, top=0.875,
+                        right=0.99, wspace=0.2, hspace=0.3)
 
 
-def model_comparison(heights, chance_val, opts):
+def model_comparison(experiment, queries, X, wcih, nofeedback, feedback,
+                     ipe_samps, kappas, opts):
+
+    nbs = 10000
+    fbtypes = ['fb', 'vfb']
+    fbratios = ['0.1', '10']
+    orders = ['C', 'E']
+
     basekwargs = {
         'align': 'center',
         'width': 1,
+        'ecolor': 'k',
     }
 
     colors = {
@@ -234,89 +254,166 @@ def model_comparison(heights, chance_val, opts):
         'Learning IPE': '#FFa500'
     }
 
-    table_models = opts['table_models']
-    table_conds = opts['table_conds']
-    orders = opts['orders']
+    data = []
+    keys = []
+    i = 0
 
-    best = np.nanargmax(heights, axis=-1)
-    best[0, 0] = 2
-    x = [0, 1, 2.5, 3.5, 5.5, 6.5, 8, 9, 11, 12]*2
+    n_kappas = ipe_samps.shape[1]
 
+    x = [0, 1, 2.5, 3.5, 5.5, 6.5, 8, 9]*2
     fig, axes = plt.subplots(1, 2)
 
-    for i in xrange(heights.size):
-        oidx, cidx, midx = np.unravel_index(i, heights.shape)
-        cond = table_conds[oidx][cidx]
-        obstype, grp, fbtype, ratio, cb = at.parse_condition(cond)
-        model = table_models[midx]
-        height = heights[oidx, cidx, midx]
+    items = itertools.product(orders, fbratios, fbtypes)
+    for order, fbratio, fbtype in items:
 
-        kwargs = basekwargs.copy()
-        kwargs.update({
-            'color': colors[model]
-        })
+        k = "H-%s-%s-%s" % (order, fbtype, fbratio)
+        keys.append(k)
+        ki = kappas.index(np.log10(float(fbratio)))
 
-        if oidx == 1 and cidx == 0:
-            kwargs['label'] = model
+        q = np.asarray(queries[k]) == float(fbratio)
+        learned = np.all(q[:, -2:], axis=-1)
 
-        axes[oidx].bar(x[i], height, **kwargs)
+        ex = experiment[k][learned]
+        n = ex.shape[0]
+        ns = n
+        if n == 0:
+            data.append(np.ones(nbs)*np.nan)
+            data.append(np.ones(nbs)*np.nan)
+            i += 2
+            continue
 
-        if best[oidx, cidx] == midx:
-            axes[oidx].text(
-                x[i], height, '*',
-                fontweight='bold',
-                ha='center', color='w')
+        rso = np.random.RandomState(2302938)
+        idx = rso.randint(0, n, ns*nbs)
+
+        for model in ('Fixed Belief IPE', 'Learning IPE'):
+            if model == 'Fixed Belief IPE':
+                fb = nofeedback
+                prior = None
+                mk = "M-%s-nfb-10" % order
+            elif model == 'Learning IPE':
+                fb = feedback[[ki]]
+                prior = np.ones((1, n_kappas))
+                prior = normalize(np.log(prior), axis=1)[1]
+                mk = "M-%s-fb-%s" % (order, fbratio)
+            else:
+                raise ValueError(model)
+
+            vals = at.block_lh(
+                {k: ex},
+                fb, ipe_samps, prior, kappas,
+                f_smooth=True, p_ignore=0.0)
+            bd = np.mean(vals[k][idx].reshape((ns, nbs)), axis=0)
+            data.append(bd)
+
+            m = np.log(0.5) * ex.shape[1]
+            chance = m
+
+            # bootstrap standard error (68% CIs)
+            lower, median, upper = np.percentile(
+                bd, [15.8655254, 50, 84.1344746])
+            lerr = median - lower
+            uerr = upper - median
+
+            kwargs = basekwargs.copy()
+            kwargs.update({
+                'color': colors[model]
+            })
+
+            oidx = orders.index(order)
+            axes[oidx].bar(
+                x[i], median - chance,
+                yerr=np.array([lerr, uerr])[:, None],
+                bottom=chance, **kwargs)
+
+            i += 1
+
+    data = np.array(data)
+    dd = data.reshape((data.shape[0]/2, 2, nbs))
+    for i in xrange(dd.shape[0]):
+        order = keys[i].split("-")[1]
+        oidx = orders.index(order)
+        p = (dd[i, 0] > dd[i, 1]).mean(axis=-1)
+        ptext = "$p"
+
+        if p < 0.05:
+            sig = "*"
+        else:
+            sig = ""
+
+        if p < 0.01:
+            ptext += "<0.01$"
+        elif p < 0.05:
+            ptext += "<0.05$"
+        elif p < 0.1:
+            ptext += "<0.1$" % p
+        else:
+            ptext += "=%.2f$" % p
+
+        text = "%s\n%s" % (ptext, sig)
+
+        axes[oidx].text(
+            x[i*2]+0.5, -24.25, text,
+            fontsize=12, horizontalalignment='center')
 
     for i, ax in enumerate(axes):
-        ax.set_ylim(-30.1, -24.8)
-        xlim = [-1, 13]
+        ylim = -29, -23.5
+        ax.set_ylim(*ylim)
+        yticks = np.arange(ylim[0], ylim[1]+1, 1)
+        ax.set_yticks(yticks)
+        ax.set_yticklabels([str(int(y)) for y in yticks])
+        xlim = [-1, 10]
         ax.set_xlim(*xlim)
+        ax.plot(xlim, [chance, chance], 'k-', lw=4)
+
+        ax.set_xticks([0.5, 3, 6, 8.5])
+        xticklabels = [
+            'Binary\n$r=0.1$', 'Visual\n$r=0.1$',
+            'Binary\n$r=10$', 'Visual\n$r=10$']
+        ax.set_xticklabels(xticklabels)
+        ax.set_title("%s order" % opts['labels'][orders[i]].capitalize(),
+                     fontsize=20, position=(0.5, 0.95))
+
+        graphing.clear_top(ax)
+        graphing.clear_right(ax)
 
         ax.xaxis.set_ticks_position('none')
         ax.yaxis.set_ticks_position('none')
         ax.tick_params(axis='x', labelsize=16)
         ax.tick_params(axis='y', labelsize=16)
-        ax.xaxis.tick_top()
 
-        ax.set_xticks([0.5, 3, 6, 8.5, 11.5])
-        xticklabels = ['Binary', 'Visual']*2
-        xticklabels += ['No f.b.']
-        ax.set_xticklabels(xticklabels)
-        ax.set_xlabel("%s order" % opts['labels'][orders[i]].capitalize(),
-                      fontsize=24)
-
-        rkwargs = {
-            'horizontalalignment': 'center',
-        }
-
-        ax.text(1.75, -24.25, r'$r_0=0.1$', **rkwargs)
-        ax.text(7.25, -24.25, r'$r_0=10$', **rkwargs)
-
-        ax.plot(xlim, [chance_val]*2, 'k--', linewidth=2, label='Chance')
-
-        graphing.clear_bottom(ax)
-        graphing.clear_right(ax)
-        graphing.clear_left(ax)
-        ax.yaxis.set_ticks_position('none')
+        ax.set_ylabel(r"Better model fit $\rightarrow$")
 
     ax1, ax2 = axes
-    ax2.set_yticks([])
-    ax2.set_yticklabels([])
-    ax1.set_yticklabels([])
-    ax1.set_ylabel(r"Better model fit $\rightarrow$")
 
-    ax2.legend(loc='lower left', frameon=False,
-               bbox_to_anchor=(0, -0.04))
+    plt.suptitle('Log likelihood of "will it fall?" judgments', fontsize=24)
+    axes[0].text(10.5, -22.4,
+                 '(Bootstrapped mean across participants who correctly '
+                 'inferred which color was heavier on trials 24 and 40)',
+                 fontsize=16, horizontalalignment='center')
 
-    ax2.text(12.75, -29.85, '* = best model fit',
-             horizontalalignment='right')
+    legend_lines = [
+        plt.Line2D((0, 1), (0, 0),
+                   color=colors.get('Fixed Belief IPE'), lw=10),
+        plt.Line2D((0, 1), (0, 0),
+                   color=colors.get('Learning IPE'), lw=10),
+        plt.Line2D((0, 1), (0, 0),
+                   color='k', lw=5),
+    ]
+    axes[1].legend(
+        legend_lines,
+        ['Fixed', 'Learning', 'Chance'],
+        loc='lower center', frameon=False, ncol=3,
+        fontsize=16, bbox_to_anchor=(0.5, 0.0))
 
-    graphing.clear_left(ax2)
+    axes[0].text(0, -25.25,
+                 r"p-values reflect "
+                 "$p(\mathrm{fixed}\ >\ \mathrm{learning})$\n"
+                 "error bars are one standard error",
+                 fontsize=12, horizontalalignment='left')
 
     fig.set_figwidth(16)
     fig.set_figheight(5.15)
 
-    plt.subplots_adjust(wspace=0.1, left=0.03, right=0.99, top=0.75,
-                        bottom=0.075)
-    plt.suptitle('Average log likelihood of human "will it fall?" judgments',
-                 fontsize=28)
+    plt.subplots_adjust(
+        wspace=0.15, left=0.05, right=0.99,
+        top=0.825, bottom=0.11)
