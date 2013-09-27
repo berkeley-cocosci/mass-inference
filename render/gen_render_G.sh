a1b4461d845f4e0be299fc0f3fbd4a37b106a6fa
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-09-27 16:24:09 -0700

    Update render script to copy stimuli to experiment dir

--- render/gen_render_G.sh
+++ render/gen_render_G.sh
@@ -22,11 +22,20 @@ function makedir() {
     fi
 }
 
-function hardlink() {
-    if [ -e "$2" ]; then
-	rm "$2"
+function remove() {
+    if [ -e "$1" ]; then
+	cmd="rm -r $1"
+	color_red "$cmd"
+	`$cmd`
     fi
-    cmd="ln $1 $2"
+}
+
+function hardlink() {
+    if [ $(uname) == "Darwin" ]; then
+	cmd="gcp -lr $1 $2"
+    else
+	cmd="cp -lr $1 $2"
+    fi
     color_green "$cmd"
     `$cmd`
 }
@@ -43,80 +52,110 @@ function render_movies() {
     python $cmd
 }
 
+function convert_videos() {
+    fmts=("mp4" "ogg" "flv" "wmv")
+    for i in $1/*.avi; do
+	if [ -e $i ]; then
+	    for fmt in ${fmts[*]}; do
+		newfile="${i%.avi}.$fmt"
+		if [ ! -e $i ]; then
+		    cmd="-loglevel error -i $i -r 30 -b 2048k -s 640x480 $newfile"
+		    color_blue "ffmpeg $cmd"
+		    ffmpeg $cmd
+		fi
+	    done
+	fi
+    done
+}
+
 ########################################################################
 
 OUTDIR="../resources/render/G"
 SSODIR="../resources/sso"
+EXPDIR="../experiment/static/stimuli"
 
 # remove old files, if they exist
-if [[ -d $OUTDIR ]]; then
+if [[ -d "$OUTDIR" ]]; then
     read -p "'$OUTDIR' already exists, do you want to remove it? (y/n) " -n 1
     echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-	cmd="rm -r $OUTDIR"
-	color_red "$cmd"
-	`$cmd`
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+	remove "$OUTDIR"
     fi
 fi
 
-# create output directories for each condition
+makedir "$OUTDIR/shared"
 makedir "$OUTDIR/vfb-10-cb0"
 makedir "$OUTDIR/vfb-10-cb1"
 makedir "$OUTDIR/vfb-0.1-cb0"
 makedir "$OUTDIR/vfb-0.1-cb1"
-makedir "$OUTDIR/shared"
 
-######################## 
-## Stability examples ##
-######################## 
+
+#################### 
+## Shared stimuli ##
+#################### 
 
 gen_render_config -o "$OUTDIR/shared/stability-examples.csv" --full "$SSODIR/stability-example-stable-F/*" "$SSODIR/stability-example-unstable-F/*"
 render_movies -c "$OUTDIR/shared/stability-examples.csv" -d "$OUTDIR/shared"
 
-
-######################
-## Pretest/posttest ##
-######################
-
-# gen_render_config -o "$OUTDIR/shared/training.csv" "$SSODIR/mass-oneshot-training-F/*"
-# render_movies -c "$OUTDIR/shared/training.csv" -d "$OUTDIR/shared"
+gen_render_config -o "$OUTDIR/shared/training.csv" "$SSODIR/mass-oneshot-training-F/*"
+render_movies -c "$OUTDIR/shared/training.csv" -d "$OUTDIR/shared"
 
 
-# hard link all the shared videos, so we don't have to render them
-# multiple times
-for i in `ls $OUTDIR/shared`; do
-    hardlink "$OUTDIR/shared/$i" "$OUTDIR/vfb-10-cb0/$i"
-    hardlink "$OUTDIR/shared/$i" "$OUTDIR/vfb-10-cb1/$i"
-    hardlink "$OUTDIR/shared/$i" "$OUTDIR/vfb-0.1-cb0/$i"
-    hardlink "$OUTDIR/shared/$i" "$OUTDIR/vfb-0.1-cb1/$i"
-done
+###################
+## Mass examples ##
+###################
+
+gen_render_config -o "$OUTDIR/vfb-10-cb0/mass-example.csv" --kappa 1.0 "$SSODIR/mass-oneshot-example-F/*"
+gen_render_config -o "$OUTDIR/vfb-10-cb1/mass-example.csv" --kappa 1.0 --flip-colors "$SSODIR/mass-oneshot-example-F/*"
+gen_render_config -o "$OUTDIR/vfb-0.1-cb0/mass-example.csv" --kappa -1.0 "$SSODIR/mass-oneshot-example-F/*"
+gen_render_config -o "$OUTDIR/vfb-0.1-cb1/mass-example.csv" --kappa -1.0 --flip-colors "$SSODIR/mass-oneshot-example-F/*"
+
+render_movies -c "$OUTDIR/vfb-10-cb0/mass-example.csv" -d "$OUTDIR/vfb-10-cb0"
+render_movies -c "$OUTDIR/vfb-10-cb1/mass-example.csv" -d "$OUTDIR/vfb-10-cb1"
+render_movies -c "$OUTDIR/vfb-0.1-cb0/mass-example.csv" -d "$OUTDIR/vfb-0.1-cb0"
+render_movies -c "$OUTDIR/vfb-0.1-cb1/mass-example.csv" -d "$OUTDIR/vfb-0.1-cb1"
 
 
-# # ###################
-# # ## Mass examples ##
-# # ###################
+################
+## Experiment ##
+################
 
-# gen_render_config -o "$OUTDIR/vfb-10-cb0/mass-example.csv" --kappa 1.0 "$SSODIR/mass-oneshot-example-F/*"
-# gen_render_config -o "$OUTDIR/vfb-10-cb1/mass-example.csv" --kappa 1.0 --flip-colors "$SSODIR/mass-oneshot-example-F/*"
-# gen_render_config -o "$OUTDIR/vfb-0.1-cb0/mass-example.csv" --kappa -1.0 "$SSODIR/mass-oneshot-example-F/*"
-# gen_render_config -o "$OUTDIR/vfb-0.1-cb1/mass-example.csv" --kappa -1.0 --flip-colors "$SSODIR/mass-oneshot-example-F/*"
+gen_render_config -o "$OUTDIR/vfb-10-cb0/experiment.csv" --kappa 1.0 "$SSODIR/mass-inference/*"
+gen_render_config -o "$OUTDIR/vfb-10-cb1/experiment.csv" --kappa 1.0 --flip-colors "$SSODIR/mass-inference/*"
+gen_render_config -o "$OUTDIR/vfb-0.1-cb0/experiment.csv" --kappa -1.0 "$SSODIR/mass-inference/*"
+gen_render_config -o "$OUTDIR/vfb-0.1-cb1/experiment.csv" --kappa -1.0 --flip-colors "$SSODIR/mass-inference/*"
 
-# render_movies -c "$OUTDIR/vfb-10-cb0/mass-example.csv" -d "$OUTDIR/vfb-10-cb0"
-# render_movies -c "$OUTDIR/vfb-10-cb1/mass-example.csv" -d "$OUTDIR/vfb-10-cb1"
-# render_movies -c "$OUTDIR/vfb-0.1-cb0/mass-example.csv" -d "$OUTDIR/vfb-0.1-cb0"
-# render_movies -c "$OUTDIR/vfb-0.1-cb1/mass-example.csv" -d "$OUTDIR/vfb-0.1-cb1"
+render_movies -c "$OUTDIR/vfb-10-cb0/experiment.csv" -d "$OUTDIR/vfb-10-cb0"
+render_movies -c "$OUTDIR/vfb-10-cb1/experiment.csv" -d "$OUTDIR/vfb-10-cb1"
+render_movies -c "$OUTDIR/vfb-0.1-cb0/experiment.csv" -d "$OUTDIR/vfb-0.1-cb0"
+render_movies -c "$OUTDIR/vfb-0.1-cb1/experiment.csv" -d "$OUTDIR/vfb-0.1-cb1"
 
 
-# # ################
-# # ## Experiment ##
-# # ################
+#####################################################
+## Convert videos and copy to experiment directory ##
+#####################################################
 
-# gen_render_config -o "$OUTDIR/vfb-10-cb0/experiment.csv" --kappa 1.0 "$SSODIR/mass-inference/*"
-# gen_render_config -o "$OUTDIR/vfb-10-cb1/experiment.csv" --kappa 1.0 --flip-colors "$SSODIR/mass-inference/*"
-# gen_render_config -o "$OUTDIR/vfb-0.1-cb0/experiment.csv" --kappa -1.0 "$SSODIR/mass-inference/*"
-# gen_render_config -o "$OUTDIR/vfb-0.1-cb1/experiment.csv" --kappa -1.0 --flip-colors "$SSODIR/mass-inference/*"
+# convert videos to different formats
+convert_videos "$OUTDIR/shared"
+convert_videos "$OUTDIR/vfb-10-cb0"
+convert_videos "$OUTDIR/vfb-10-cb1"
+convert_videos "$OUTDIR/vfb-0.1-cb0"
+convert_videos "$OUTDIR/vfb-0.1-cb1"
 
-# render_movies -c "$OUTDIR/vfb-10-cb0/experiment.csv" -d "$OUTDIR/vfb-10-cb0"
-# render_movies -c "$OUTDIR/vfb-10-cb1/experiment.csv" -d "$OUTDIR/vfb-10-cb1"
-# render_movies -c "$OUTDIR/vfb-0.1-cb0/experiment.csv" -d "$OUTDIR/vfb-0.1-cb0"
-# render_movies -c "$OUTDIR/vfb-0.1-cb1/experiment.csv" -d "$OUTDIR/vfb-0.1-cb1"
+# hard link all the shared files, so we don't have to render/convert
+# them multiple times
+hardlink "$OUTDIR/shared/*" "$OUTDIR/vfb-10-cb0/"
+hardlink "$OUTDIR/shared/*" "$OUTDIR/vfb-10-cb1/"
+hardlink "$OUTDIR/shared/*" "$OUTDIR/vfb-0.1-cb0/"
+hardlink "$OUTDIR/shared/*" "$OUTDIR/vfb-0.1-cb1/"
+
+read -p "Copy to experiment stimuli directory? (y/n) " -n 1
+echo
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+    remove "$EXPDIR"
+    makedir "$EXPDIR"
+    hardlink "$OUTDIR/vfb-10-cb0" "$EXPDIR/vfb-10-cb0"
+    hardlink "$OUTDIR/vfb-10-cb1" "$EXPDIR/vfb-10-cb1"
+    hardlink "$OUTDIR/vfb-0.1-cb0" "$EXPDIR/vfb-0.1-cb0"
+    hardlink "$OUTDIR/vfb-0.1-cb1" "$EXPDIR/vfb-0.1-cb1"
+fi
