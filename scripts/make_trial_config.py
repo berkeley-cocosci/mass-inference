eeb284f6de377213ba0a0040e4d4b5f002517eb7
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-10-11 10:49:36 -0700

    Update make_trial_config script to handle two experiment blocks

--- scripts/make_trial_config.py
+++ scripts/make_trial_config.py
@@ -4,18 +4,41 @@ import numpy as np
 import pandas as pd
 from path import path
 
+conditions = {
+    "0": {
+        "pretest": "shared",
+        "experimentA": "nfb-0.1",
+        "experimentB": "vfb-0.1",
+        "posttest": "shared",
+    },
+
+    "1": {
+        "pretest": "shared",
+        "experimentA": "nfb-0.1",
+        "experimentB": "vfb-10",
+        "posttest": "shared",
+    },
+
+    "2": {
+        "pretest": "shared",
+        "experimentA": "nfb-10",
+        "experimentB": "vfb-0.1",
+        "posttest": "shared",
+    },
+
+    "3": {
+        "pretest": "shared",
+        "experimentA": "nfb-10",
+        "experimentB": "vfb-10",
+        "posttest": "shared",
+    },
+}
 
 render_dir = path("../resources/render/G/")
 json_dir = path("../experiment/static/json/")
 
-conditions = [str(x.name) for x in render_dir.listdir()]
-conditions = [x for x in conditions if x != "shared"]
-
-base_conds = sorted(set(["-".join(x.split("-")[:-1]) for x in conditions]))
-base_conds = dict(enumerate(base_conds))
-
 with open(json_dir.joinpath("conditions.json"), "w") as fh:
-    json.dump(base_conds, fh, indent=2)
+    json.dump(conditions, fh, indent=2, allow_nan=False)
 
 DBPATH = path("../resources/sso/metadata.db")
 tbl = dbtools.Table(DBPATH, "stability")
@@ -28,66 +51,79 @@ def get_meta(stim, kappa):
     assert len(meta) == 1
     return meta
 
-for condition in conditions:
-    render_configs = render_dir.joinpath(condition).glob("*.csv")
 
-    fb, ratio, cb = condition.split("-")
-    if cb == "cb0":
-        cb = 0
-    elif cb == "cb1":
-        cb = 1
-    else:
-        raise ValueError("invalid counterbalance: %s" % cb)
-
+for condition, maps in conditions.iteritems():
     config = {}
-    for conf_path in render_configs:
-        seed = abs(hash(conf_path.name))
-        print conf_path, seed
+    for phase, pth in maps.iteritems():
+        for cb in [0, 1]:
 
-        conf = pd.DataFrame.from_csv(conf_path).reset_index()
-        conf.stimulus = conf.stimulus.map(lambda x: path(x).namebase)
+            if phase in ("pretest", "posttest"):
+                render_config = render_dir.joinpath(
+                    pth, "%s.csv" % phase)
 
-        conf['feedback'] = fb
-        conf['ratio'] = ratio
-        conf['counterbalance'] = cb
-        conf['fall? query'] = True
-        conf['mass? query'] = True
-        conf['color0'] = 'yellow'
-        conf['color1'] = 'red'
+                fb = "vfb"
+                ratio = 1
+                ask_fall = True
+                ask_mass = False
 
-        is_orig = conf.stimtype == "original"
-        conf["mass? query"][is_orig] = False
-        conf["color0"][is_orig] = None
-        conf["color1"][is_orig] = None
+            else:
+                render_config = render_dir.joinpath(
+                    "%s-cb%d" % (pth, cb), "%s.csv" % phase)
 
-        meta = pd.concat(map(get_meta, conf.stimulus, conf.kappa))
-        meta['stable'] = meta['stable'].astype('bool')
-        meta = meta.drop('dataset', axis=1)
+                fb, ratio = pth.split("-")
+                if phase == "experimentA":
+                    ask_fall = True
+                    ask_mass = False
+                elif phase == "experimentB":
+                    ask_fall = False
+                    ask_mass = True
 
-        conf = pd.merge(conf, meta, on=['stimulus', 'kappa'])
-        conf = (conf
-                .drop(
-                    ["full_render", "stimtype", "kappa", "flip_colors"],
-                    axis=1)
-                .set_index('stimulus')
-                .sort()
-                .reset_index())
+            seed = abs(hash(phase))
+            print condition, phase, seed
 
-        idx = np.array(conf.index)
-        rso.seed(seed)
-        rso.shuffle(idx)
+            conf = pd.DataFrame.from_csv(render_config).reset_index()
+            conf.stimulus = conf.stimulus.map(lambda x: path(x).namebase)
 
-        shuffled = conf.reindex(idx).reset_index(drop=True)
-        shuffled.index.name = "trial"
-        shuffled = shuffled.reset_index()
+            conf['feedback'] = fb
+            conf['ratio'] = ratio
+            conf['counterbalance'] = cb
+            conf['fall? query'] = ask_fall
+            conf['mass? query'] = ask_mass
 
-        dicts = shuffled.T.to_dict()
-        trials = [dicts[i] for i in shuffled.index]
+            meta = pd.concat(map(get_meta, conf.stimulus, conf.kappa))
+            meta['stable'] = meta['stable'].astype('bool')
+            meta = meta.drop('dataset', axis=1)
 
-        if len(trials) == 1:
-            trials = trials[0]
+            conf = pd.merge(conf, meta, on=['stimulus', 'kappa'])
+            conf = (conf
+                    .drop(
+                        ["full_render", "stimtype", "flip_colors"],
+                        axis=1)
+                    .set_index('stimulus')
+                    .sort()
+                    .reset_index())
 
-        config[conf_path.namebase] = trials
+            idx = np.array(conf.index)
+            rso.seed(seed)
+            rso.shuffle(idx)
+
+            shuffled = conf.reindex(idx).reset_index(drop=True)
+            shuffled.index.name = "trial"
+            shuffled = shuffled.reset_index()
+
+            dicts = shuffled.T.to_dict()
+            trials = [dicts[i] for i in shuffled.index]
+
+            for trial in trials:
+                for key, val in trial.iteritems():
+                    if isinstance(val, float) and np.isnan(val):
+                        if key in ("color0", "color1"):
+                            trial[key] = None
+
+            if len(trials) == 1:
+                trials = trials[0]
+
+            config[phase] = trials
 
     with open(json_dir.joinpath("%s.json" % condition), "w") as fh:
-        json.dump(config, fh, indent=2)
+        json.dump(config, fh, indent=2, allow_nan=False)
