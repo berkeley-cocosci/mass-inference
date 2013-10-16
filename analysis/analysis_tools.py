e9854709f98fc8be30c6fd0710e61a7045f33ad7
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-10-16 10:16:27 -0700

    Move datapackage functions to snippets

--- analysis/analysis_tools.py
+++ analysis/analysis_tools.py
@@ -1,10 +1,6 @@
-import os
 import pandas as pd
-import json
-import hashlib
 import numpy as np
 import scipy
-from path import path
 
 import matplotlib
 import matplotlib.cm as cm
@@ -16,6 +12,7 @@ from statsmodels.stats.proportion import proportions_chisquare
 import model_observer as mo
 
 from snippets.safemath import normalize
+from snippets.datapackage import md5, load_datapackage
 
 CONDITION = ['order', 'feedback', 'ratio']
 PID = CONDITION + ['pid']
@@ -23,65 +20,6 @@ PARAMS = ['sigma', 'phi', 'kappa']
 
 binom_mean = mo.binom_mean
 binom_std = mo.binom_std
-
-
-def md5(data_path):
-    # we need to compute the md5 sum one chunk at a time, because some
-    # files are too large to fit in memory
-    md5 = hashlib.md5()
-    with open(data_path, 'r') as fh:
-        while True:
-            chunk = fh.read(128)
-            if not chunk:
-                break
-            md5.update(chunk)
-    data_hash = md5.hexdigest()
-    return data_hash
-
-
-def load_datapackage(pth, verify_checksums=True):
-    dpjson_pth = path(os.path.join(pth, "datapackage.json"))
-    if not dpjson_pth.exists():
-        raise IOError("No metadata file datapackage.json")
-    with open(dpjson_pth, "r") as fh:
-        dpjson = json.load(fh)
-
-    resource_info = dpjson['resources']
-    resources = {}
-    for resource in resource_info:
-        if resource.get('path', None):
-            resource_pth = path(os.path.join(pth, resource['path']))
-
-            # check format and load data
-            if resource['format'] == 'csv':
-                data = pd.DataFrame.from_csv(resource_pth)
-            elif resource['format'] == 'json':
-                with open(resource_pth, "r") as fh:
-                    data = json.load(fh)
-            elif resource['format'] == 'npy':
-                data = np.load(resource_pth, mmap_mode='c')
-            else:
-                raise ValueError("unsupported format: %s" % resource['format'])
-
-            # check the file size
-            size = resource_pth.getsize()
-            if size != resource['bytes']:
-                raise IOError("resource has changed size on disk")
-
-            # load the raw data and check md5
-            if verify_checksums:
-                if md5(resource_pth) != resource['hash']:
-                    raise IOError("resource checksum has changed")
-
-        elif 'data' in resource:
-            data = resource['data']
-
-        else:
-            raise ValueError("malformed resource")
-
-        resources[resource['name']] = data
-
-    return resources
 
 
 def uncounterbalance(arr):
