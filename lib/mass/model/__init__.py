aeaa928a7e760b37a475df849c3bbb615fc787b6
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-11-30 17:33:00 -0800

    Add EmpiricalIPE

--- lib/mass/model/__init__.py
+++ lib/mass/model/__init__.py
@@ -1,6 +1,7 @@
 from ipe import IPE
+from empirical_ipe import EmpiricalIPE
 from feedback import Feedback, NoFeedback
 from observer_model import ObserverModel
 from fall_model import FallModel
 
-__all__ = ['IPE', 'Feedback', 'NoFeedback', 'ObserverModel', 'FallModel']
+__all__ = ['IPE', 'EmpiricalIPE', 'Feedback', 'NoFeedback', 'ObserverModel', 'FallModel']
