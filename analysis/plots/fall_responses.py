94a68d711d6898ecb61608e13f70542951674fca
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-02-23 17:16:54 -0800

    Some plot formatting

--- analysis/plots/fall_responses.py
+++ analysis/plots/fall_responses.py
@@ -11,7 +11,7 @@ def plot_block(block, results_path, fig_path):
     groups = results.set_index(['stimulus', 'kappa0'])\
                     .groupby(['block', 'species'])
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
     colors = {
         -1.0: 'r',
@@ -26,32 +26,49 @@ def plot_block(block, results_path, fig_path):
         .unstack('kappa0')
     y = y.unstack('kappa0')
 
+    ax1.errorbar(
+        y[-1.0], y[1.0],
+        xerr=[y_lerr[-1.0], y_uerr[-1.0]],
+        yerr=[y_lerr[1.0], y_uerr[1.0]],
+        marker='o', color='k', ls='', ms=8)
+    ax1.set_xlim(1, 7)
+    ax1.set_ylim(1, 7)
+    ax1.set_xlabel(r"Human ($r_0=0.1$)")
+    ax1.set_ylabel(r"Human ($r_0=10.0$)")
+
+    ax2.plot([x[-1.0], x[1.0]], [y[-1.0], y[1.0]], 'k-')
+    ax3.plot([x[0.0], x[0.0]], [y[-1.0], y[1.0]], 'k-')
+
     for kappa0 in (-1.0, 1.0):
-        ax1.errorbar(
+        ax2.errorbar(
             x[kappa0], y[kappa0],
             yerr=[y_lerr[kappa0], y_uerr[kappa0]],
             marker='o', color=colors[kappa0], ms=8, ls='',
             label=r"$r_0=%.1f$" % 10 ** kappa0)
-        ax2.errorbar(
+        ax3.errorbar(
             x[0.0], y[kappa0],
             yerr=[y_lerr[kappa0], y_uerr[kappa0]],
             marker='o', color=colors[kappa0], ms=8, ls='',
             label=r"$r_0=%.1f$" % 10 ** kappa0)
 
-    ax1.plot([x[-1.0], x[1.0]], [y[-1.0], y[1.0]], 'k-')
-    ax2.plot([x[0.0], x[0.0]], [y[-1.0], y[1.0]], 'k-')
+    ax2.set_xlabel("Mass-sensitive IPE")
+    ax3.set_xlabel("Mass-insensitive IPE")
 
-    ax1.legend(title='Mass ratio', loc='best')
+    ax3.legend(title='Mass ratio', loc='best')
 
-    ax1.set_xlim(0, 1)
-    ax1.set_ylim(1, 7)
-    ax1.set_xlabel("IPE")
-    ax2.set_xlabel("IPE")
-    ax1.set_ylabel("Human")
-    ax1.set_title("Mass-sensitive predictions")
-    ax2.set_title("Mass-insensitive predictions")
+    for ax in (ax2, ax3):
+        ax.set_xlim(0, 1)
+        ax.set_ylim(1, 7)
+        ax.set_ylabel("Human")
 
-    fig.set_figwidth(8)
+    for ax in (ax1, ax2, ax3):
+        util.clear_right(ax)
+        util.clear_top(ax)
+        util.outward_ticks(ax)
+        ax.set_axis_bgcolor('0.95')
+
+    fig.set_figheight(3.5)
+    fig.set_figwidth(12)
     plt.draw()
     plt.tight_layout()
 
