60595c48eab31d5eb97f62d1bf1a186e2e6219df
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-02-24 11:39:33 -0800

    Remove pymc stuff, some renaming

--- lib/mass/model/__init__.py
+++ lib/mass/model/__init__.py
@@ -1,7 +1,5 @@
 from ipe import IPE
 from empirical_ipe import EmpiricalIPE
 from feedback import Feedback, NoFeedback
-from observer_model import ObserverModel
-from fall_model import FallModel
 
-__all__ = ['IPE', 'EmpiricalIPE', 'Feedback', 'NoFeedback', 'ObserverModel', 'FallModel']
+__all__ = ['IPE', 'EmpiricalIPE', 'Feedback', 'NoFeedback']
