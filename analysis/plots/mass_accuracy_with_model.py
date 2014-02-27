818d377210d7bb6c3931bf563286d7818043f321
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-02-27 13:06:04 -0800

    Combine mass_accuracy_with_model plots into a single plot

--- analysis/plots/mass_accuracy_with_model.py
+++ analysis/plots/mass_accuracy_with_model.py
@@ -5,36 +5,43 @@ import pandas as pd
 import util
 
 
-def plot_class(cls, results_path, fig_path):
+def plot(results_path, fig_path):
 
-    mass_responses = pd\
+    responses = pd\
         .read_csv(results_path.joinpath('mass_responses.csv'))\
-        .groupby('class')\
-        .get_group(cls)
+        .groupby('class')
+    classes = ['learning', 'static', 'chance', 'best']
 
     colors = {
         'human': 'k',
-        'empirical': 'r',
-        'ipe': 'b'
+        'model': 'r',
     }
 
-    fig, ax = plt.subplots()
-    for species, sdf in mass_responses.groupby('species'):
-        x = sdf['trial']
-        y = sdf['median']
-        yl = sdf['lower']
-        yu = sdf['upper']
+    fig, axes = plt.subplots(1, len(classes))
+    for i, cls in enumerate(classes):
+        df = responses.get_group(cls)
+        ax = axes[i]
 
-        ax.fill_between(x, yl, yu, alpha=0.3, color=colors[species])
-        ax.plot(x, y, color=colors[species], lw=2, label=species)
+        for species, sdf in df.groupby('species'):
+            if species == 'empirical':
+                species = 'model'
 
-    ax.set_xlim(1, 20)
-    ax.set_ylim(0, 1)
-    ax.set_xlabel("Trial")
-    ax.set_ylabel("Fraction correct")
+            x = sdf['trial']
+            y = sdf['median']
+            yl = sdf['lower']
+            yu = sdf['upper']
 
-    ax.legend(loc='best')
-    fig.set_figwidth(6)
+            ax.fill_between(x, yl, yu, alpha=0.3, color=colors[species])
+            ax.plot(x, y, color=colors[species], lw=2, label=species)
+
+        ax.set_xlim(1, 20)
+        ax.set_ylim(0, 1)
+        ax.set_xlabel("Trial")
+        ax.set_ylabel("Fraction correct")
+        ax.set_title(cls.capitalize())
+
+    axes[-1].legend(loc='best')
+    fig.set_figwidth(16)
     plt.draw()
     plt.tight_layout()
 
@@ -42,15 +49,6 @@ def plot_class(cls, results_path, fig_path):
             for ext in ('png', 'pdf')]
     for pth in pths:
         util.save(pth, close=False)
-    return pths
-
-
-def plot(results_path, fig_path):
-    pths = []
-    responses = pd.read_csv(results_path.joinpath('mass_responses.csv'))
-    lhtypes = list(responses['likelihood'].unique())
-    for lhtype in lhtypes:
-        pths.extend(plot_lhtype(lhtype, results_path, fig_path))
     return pths
 
 
