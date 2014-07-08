47c559868150d4d2a9ec3830c61bbabb0b441dc6
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-07-08 15:09:09 -0700

    Add mass_accuracy_by_stimulus_corrs

--- analysis/analyses/__init__.py
+++ analysis/analyses/__init__.py
@@ -9,6 +9,7 @@ import model_belief
 import model_belief_agg
 import mass_accuracy
 import mass_accuracy_by_stimulus
+import mass_accuracy_by_stimulus_corrs
 import mass_accuracy_by_trial
 import mass_responses_by_stimulus
 import mass_responses_by_stimulus_corrs
@@ -30,6 +31,7 @@ __all__ = [
     'model_belief_agg',
     'mass_accuracy',
     'mass_accuracy_by_stimulus',
+    'mass_accuracy_by_stimulus_corrs',
     'mass_responses_by_stimulus',
     'mass_responses_by_stimulus_corrs',
     'mass_accuracy_by_trial',
