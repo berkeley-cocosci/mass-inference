3667a0a6fe190428648ad7370f6529a78252d3f0
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-07-08 16:58:48 -0700

    Update annex helper scripts to be more useful

--- bin/annex_unlock.sh
+++ bin/annex_unlock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e 
 
 DATA_PATH=data
-git annex unlock $DATA_PATH/human-raw/mass_inference-I/
+git annex unlock $DATA_PATH/human/mass_inference-G.dpkg
+git annex unlock $DATA_PATH/human/mass_inference-H.dpkg
 git annex unlock $DATA_PATH/human/mass_inference-I.dpkg
 git annex unlock $DATA_PATH/human/mass_inference-merged.dpkg
-git annex unlock $DATA_PATH/human/workers.db
