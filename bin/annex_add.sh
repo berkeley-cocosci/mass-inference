db8f8c228daa50c31182102d5ec9d68837925180
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-07-11 16:30:35 -0700

    Include sim scripts in annex helper scripts

--- bin/annex_add.sh
+++ bin/annex_add.sh
@@ -2,6 +2,7 @@
 
 DATA_PATH=data
 git annex add resources/sso/metadata.db
+git annex add resources/sim-scripts/mass_inference-I*
 git annex add $DATA_PATH/sim-raw/mass_inference-I*/*.tar.gz
 git annex add $DATA_PATH/model-raw/mass_inference-I*/*.npy
 git add $DATA_PATH/model-raw/mass_inference-I*/datapackage.json
--- bin/annex_unlock.sh
+++ bin/annex_unlock.sh
@@ -2,6 +2,7 @@
 
 DATA_PATH=data
 git annex unlock resources/sso/metadata.db
+git annex unlock resources/sim-scripts/mass_inference-I*
 git annex unlock $DATA_PATH/sim-raw/mass_inference-I*
 git annex unlock $DATA_PATH/model-raw/mass_inference-I*
 git annex unlock $DATA_PATH/model/mass_inference-I*
