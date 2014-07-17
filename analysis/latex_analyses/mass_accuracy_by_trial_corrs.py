7f87b6bbb9c3be8f13027b7d7007bb3f4d1ec4df
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-07-17 12:29:53 -0700

    Add latex results for mass accuracy by trial corrs

---
+++ analysis/latex_analyses/mass_accuracy_by_trial_corrs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import util
+import pandas as pd
+
+
+def run(latex_path, results_path):
+    results = pd.read_csv(
+        results_path.joinpath("mass_accuracy_by_trial_corrs.csv"))
+
+    results = results\
+        .set_index(['kappa0', 'version', 'num_mass_trials'])\
+        .ix['all']\
+        .ix[[('G', 8), ('H', 20), ('I', -1)]]
+
+    replace = {
+        'H': 'One',
+        'G': 'Two',
+        'I': 'Three'
+    }
+
+    fh = open(latex_path, "w")
+
+    for (version, num), corrs in results.iterrows():
+        cmdname = "Exp{}MassTrialCorr".format(
+            replace[version])
+        cmd = util.latex_pearson.format(**corrs)
+        fh.write(util.newcommand(cmdname, cmd))
+
+    fh.close()
+    return latex_path
+
+
+if __name__ == "__main__":
+    util.run_analysis(run, sys.argv[1])
