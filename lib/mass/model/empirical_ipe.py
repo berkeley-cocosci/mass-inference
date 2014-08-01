272f703f41e366c454f23f52fa6ec4fd0d33dc3e
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-07-31 18:50:02 -0700

    Make empirical ipe be consistent with IPE

--- lib/mass/model/empirical_ipe.py
+++ lib/mass/model/empirical_ipe.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import scipy.special
+import pandas as pd
 
 from .util import LazyProperty
 
@@ -9,33 +10,61 @@ class EmpiricalIPE(object):
         self.data = data.copy().rename(columns={'kappa0': 'kappa'})
         self.data['fall? response'] = self.data['fall? response'] - 1
 
-    def _sample_kappa_mean(self, data):
+    def _sample_kappa_alpha(self, data):
         samps = data.pivot(
             index='pid',
             columns='kappa',
             values='fall? response')
         alpha = samps.sum() + 0.5
+        return alpha
+
+    def _sample_kappa_beta(self, data):
+        samps = data.pivot(
+            index='pid',
+            columns='kappa',
+            values='fall? response')
         beta = (6 - samps).sum() + 0.5
+        return beta
+
+    @LazyProperty
+    def alpha(self):
+        return self.data.groupby('stimulus').apply(self._sample_kappa_alpha)
+
+    @LazyProperty
+    def beta(self):
+        return self.data.groupby('stimulus').apply(self._sample_kappa_beta)
+
+    @LazyProperty
+    def P_fall_mean(self):
+        alpha = self.alpha
+        beta = self.beta
         mean = alpha / (alpha + beta)
         return mean
 
-    def _sample_kappa_var(self, data):
-        samps = data.pivot(
-            index='pid',
-            columns='kappa',
-            values='fall? response')
-        alpha = samps.sum() + 0.5
-        beta = (6 - samps).sum() + 0.5
+    @LazyProperty
+    def P_fall_var(self):
+        alpha = self.alpha
+        beta = self.beta
         var = (alpha * beta) / ((alpha + beta) ** 2 * (alpha + beta + 1))
         return var
 
     @LazyProperty
-    def P_fall_mean(self):
-        return self.data.groupby('stimulus').apply(self._sample_kappa_mean)
-
-    @LazyProperty
-    def P_fall_var(self):
-        return self.data.groupby('stimulus').apply(self._sample_kappa_var)
+    def P_fall_stats(self):
+        alpha = self.alpha.stack()
+        beta = self.beta.stack()
+        stats = scipy.special.btdtri(
+            np.asarray(alpha)[:, None],
+            np.asarray(beta)[:, None],
+            [0.025, 0.5, 0.975])
+        stats = pd.DataFrame(
+            stats,
+            index=alpha.index,
+            columns=['lower', 'median', 'upper'])
+        stats.columns.name = 'stat'
+        stats = stats\
+            .stack()\
+            .unstack('kappa')
+        return stats
 
     @property
     def P_fall_smooth(self):
