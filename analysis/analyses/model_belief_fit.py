4f7b08f46aae626b995cf50d89d584605fd5049a
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-10-13 10:14:26 -0700

    Update model_belief_fit to use HDF5

--- analysis/analyses/model_belief_fit.py
+++ analysis/analyses/model_belief_fit.py
@@ -4,7 +4,6 @@ import sys
 import util
 import pandas as pd
 import numpy as np
-from path import path
 from scipy.optimize import minimize
 
 def log_laplace(x, mu=0, b=1):
@@ -44,19 +43,18 @@ def logistic_regression(X, y, prior_func, verbose=False):
     return float(best['x'])
 
 def fit_responses(df, prior_func, verbose=False):
-    y = np.asarray(df['responses'])
-    X = np.asarray(df['llr'])
-    
+    df2 = df.dropna()
+    y = np.asarray(df2['responses'])
+    X = np.asarray(df2['llr'])
+
     if 'chance' in df.name:
         B = 0
     else:
         B = logistic_regression(X, y, prior_func, verbose)
-        
-    f = X * B
-    mu = 1.0 / (1 + np.exp(-f))
-        
+
+    mu = 1.0 / (1 + np.exp(-(df['llr'] * B)))
+
     new_df = df.copy()
-    new_df['f'] = f
     new_df['B'] = B
     new_df['p'] = mu.copy()
     new_df['p correct'] = mu.copy()
@@ -66,113 +64,84 @@ def fit_responses(df, prior_func, verbose=False):
 
     return new_df
 
-def load(results_path):
+def run(results_path, seed):
     human = util.load_human()
 
     # load in raw human mass responses
     responses = human['C']\
-        .set_index(['version', 'pid', 'trial', 'stimulus'])['mass? response']\
-        .dropna()\
+        .set_index(['version', 'pid', 'trial', 'stimulus'])[['kappa0', 'mass? response']]\
         .sortlevel()
-    responses = (responses + 1) / 2
-
-    # load in human mass accuracy
-    correct = human['C']\
-        .set_index(['version', 'pid', 'trial', 'stimulus'])['mass? correct']\
-        .dropna()\
-        .sortlevel()
-
-    # load in the correct hypothesis
-    kappa0 = human['C']\
-        .set_index(['version', 'pid', 'trial', 'stimulus'])['kappa0']\
-        .ix[correct.index]
+    responses['mass? response'] = (responses['mass? response'] + 1) / 2
 
     # load in raw model belief
-    belief = pd.read_csv("results/model_belief_by_trial.csv")
-
-    # TODO: need to run this for all priors, not just empirical
-    query = util.get_query()
-    llh = belief\
-        .groupby(['likelihood', 'query'])\
-        .get_group(('empirical', query))\
-        .drop(['sigma', 'phi'], axis=1)\
-        .set_index(['version', 'model', 'pid', 'trial', 'stimulus', 'hypothesis'])['logp']\
-        .unstack('hypothesis')\
-        .sortlevel()
-
-    # compute the log likelihood ratio between the two hypotheses
-    llr = (llh[1] - llh[-1])\
-        .unstack('model')\
-        .ix[responses.index]
-
-    # build up a dataframe with all the relevant information
-    model = llr.copy()
-    model['responses'] = responses
-    model['correct'] = correct
-    model['kappa0'] = kappa0
-    model = model\
-        .set_index(['responses', 'correct', 'kappa0'], append=True)\
-        .stack()\
-        .reset_index(['responses', 'correct', 'kappa0'])\
-        .rename(columns={0: 'llr'})
-    model.index.names = ['version', 'pid', 'trial', 'stimulus', 'model']
-    model = model\
-        .reorder_levels(['version', 'model', 'pid', 'trial', 'stimulus'])\
-        .sortlevel()
-
-    return model
-
-def run(results_path, seed):
-    np.random.seed(seed)
-    model = load(results_path)
+    old_store = pd.HDFStore('results/model_belief_by_trial.h5')
+    store = pd.HDFStore(results_path, mode='w')
 
     # L2 regularization is equivalent to using a laplace prior
     laplace_prior = make_prior(log_laplace, mu=1, b=1)
 
-    # # use L2 logistic regression to fit a global parameter to all
-    # # participants in aggregate
-    # res_all = model\
-    #     .groupby(level=['version', 'model'])\
-    #     .apply(fit_responses, laplace_prior)
-    # params_all = res_all\
-    #     .reset_index()[['version', 'model', 'B']]\
-    #     .drop_duplicates()\
-    #     .set_index(['version', 'model'])
+    for key in old_store.keys():
+        print key
+        if key.split('/')[-1] == 'param_ref':
+            store.append(key, old_store[key])
+            continue
 
-    # use L2 logistic regression to fit parameters individually to
-    # each participant in version G
-    res_G = model\
-        .groupby(level='version')\
-        .get_group('G')\
-        .groupby(level=['model', 'pid'])\
-        .apply(fit_responses, laplace_prior)
-    params_G = res_G\
-        .reset_index()[['model', 'pid', 'B']]\
-        .drop_duplicates()\
-        .set_index(['model', 'pid'])['B']
+        llh = old_store[key]\
+            .set_index(['stimulus', 'model'], append=True)['logp']\
+            .unstack('hypothesis')
 
-    # use the parameters fit to participants in version G as a prior
-    # over parameters, and then fit parameters to each participant
-    empirical_priors = params_G.groupby(level='model').apply(
-        lambda x: make_prior(kde, np.asarray(x), 0.1))
-    res_ind = model\
-        .groupby(level=['version', 'model', 'pid'])\
-        .apply(lambda x: fit_responses(x, empirical_priors[x.name[1]]))
-    params_ind = res_ind\
-        .reset_index()[['version', 'model', 'pid', 'B']]\
-        .drop_duplicates()\
-        .set_index(['version', 'model', 'pid'])
+        # compute the log likelihood ratio between the two hypotheses
+        llr = llh[-1] - llh[1]
 
-    results = res_G.reset_index()
-    results['version'] = 'G'
-    results = pd.concat([results, res_ind.drop('G', level='version').reset_index()])
-    results['likelihood'] = 'empirical'
-    results['query'] = util.get_query()
-    results = results\
-        .set_index(
-            ['model', 'likelihood', 'version', 'trial', 'query',
-             'kappa0', 'pid', 'stimulus'])[['p', 'p correct', 'B']]\
-        .sortlevel()
+        model = llr\
+            .reset_index()\
+            .rename(columns={0: 'llr'})\
+            .set_index(['version', 'pid', 'trial', 'stimulus'])
+        model['responses'] = responses['mass? response'].copy()
+        model['kappa0'] = responses['kappa0'].copy()
+        model = model\
+            .reset_index()\
+            .set_index(['model', 'version', 'pid', 'trial', 'stimulus'])\
+            .sortlevel()
+
+        # use L2 logistic regression to fit parameters individually to
+        # each participant in version G
+        res_G = model\
+            .groupby(level='version')\
+            .get_group('G')\
+            .groupby(level=['model', 'pid'])\
+            .apply(fit_responses, laplace_prior)
+        params_G = res_G\
+            .reset_index()[['model', 'version', 'kappa0', 'pid', 'B']]\
+            .drop_duplicates()\
+            .set_index(['model', 'version', 'kappa0', 'pid'])['B']
+
+        # use the parameters fit to participants in version G as a prior
+        # over parameters, and then fit parameters to each participant
+        empirical_priors = params_G.groupby(level='model').apply(
+            lambda x: make_prior(kde, np.asarray(x), 0.1))
+        res_ind = model\
+            .drop('G', level='version')\
+            .groupby(level=['version', 'model', 'pid'])\
+            .apply(lambda x: fit_responses(x, empirical_priors[x.name[1]]))
+        params_ind = res_ind\
+            .reset_index()[['model', 'version', 'kappa0', 'pid', 'B']]\
+            .drop_duplicates()\
+            .set_index(['model', 'version', 'kappa0', 'pid'])['B']
+
+        results = res_G.reset_index()
+        results['version'] = 'G'
+        results = pd\
+            .concat([results, res_ind.reset_index()])\
+            .dropna()\
+            .set_index(['model', 'version', 'kappa0', 'pid', 'trial', 'stimulus'])\
+            .sortlevel()\
+            .drop('B', axis=1)
+
+        params = pd.concat([params_G, params_ind]).sortlevel()
+
+        store.append("{}/belief".format(key), results)
+        store.append("{}/params".format(key), params)
 
     results.to_csv(results_path)
 
