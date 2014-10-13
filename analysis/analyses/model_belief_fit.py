ecc1d9301206864e5a339159c499f98f7343f167
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-10-13 16:38:19 -0700

    Make model_belief_fit parallelized

--- analysis/analyses/model_belief_fit.py
+++ analysis/analyses/model_belief_fit.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 from path import path
 from scipy.optimize import minimize
+from IPython.parallel import Client, require
 
 
 def log_laplace(x, mu=0, b=1):
@@ -66,6 +67,78 @@ def fit_responses(df, prior_func, verbose=False):
     return new_df
 
 
+@require('numpy', 'pandas', 'sys')
+def task(args):
+    key, responses, old_store_pth, pth = args
+    print key
+
+    sys.path.append(pth)
+    from analyses import util
+    from analyses import model_belief_fit as mbf
+    pd = pandas
+    np = numpy
+
+    old_store = pd.HDFStore(old_store_pth, mode='r')
+    llh = old_store[key]\
+        .set_index(['stimulus', 'model'], append=True)['logp']\
+        .unstack('hypothesis')
+
+    # L2 regularization is equivalent to using a laplace prior
+    laplace_prior = mbf.make_prior(mbf.log_laplace, mu=1, b=1)
+
+    # compute the log likelihood ratio between the two hypotheses
+    llr = llh[-1] - llh[1]
+
+    model = llr\
+        .reset_index()\
+        .rename(columns={0: 'llr'})\
+        .set_index(['version', 'pid', 'trial', 'stimulus'])
+    model['responses'] = responses['mass? response']
+    model['kappa0'] = responses['kappa0']
+    model = model\
+        .reset_index()\
+        .set_index(['model', 'version', 'pid', 'trial', 'stimulus'])\
+        .sortlevel()
+
+    # use L2 logistic regression to fit parameters individually to
+    # each participant in version G
+    res_G = model\
+        .groupby(level='version')\
+        .get_group('G')\
+        .groupby(level=['model', 'pid'])\
+        .apply(mbf.fit_responses, laplace_prior)
+    params_G = res_G\
+        .reset_index()[['model', 'version', 'kappa0', 'pid', 'B']]\
+        .drop_duplicates()\
+        .set_index(['model', 'version', 'kappa0', 'pid'])['B']
+
+    # use the parameters fit to participants in version G as a prior
+    # over parameters, and then fit parameters to each participant
+    empirical_priors = params_G.groupby(level='model').apply(
+        lambda x: mbf.make_prior(util.kde, np.asarray(x), 0.1))
+    res_ind = model\
+        .drop('G', level='version')\
+        .groupby(level=['version', 'model', 'pid'])\
+        .apply(lambda x: mbf.fit_responses(x, empirical_priors[x.name[1]]))
+    params_ind = res_ind\
+        .reset_index()[['model', 'version', 'kappa0', 'pid', 'B']]\
+        .drop_duplicates()\
+        .set_index(['model', 'version', 'kappa0', 'pid'])['B']
+
+    results = res_G.reset_index()
+    results['version'] = 'G'
+    results = pd\
+        .concat([results, res_ind.reset_index()])\
+        .dropna()\
+        .set_index(['model', 'version', 'kappa0', 'pid', 'trial', 'stimulus'])\
+        .sortlevel()\
+        .drop('B', axis=1)
+
+    params = pd.concat([params_G, params_ind]).sortlevel()
+
+    return key, results, params
+
+
 def run(results_path, seed):
     human = util.load_human()
 
@@ -76,74 +149,32 @@ def run(results_path, seed):
     responses['mass? response'] = (responses['mass? response'] + 1) / 2
 
     # load in raw model belief
-    old_store = pd.HDFStore(path(results_path).dirname().joinpath(
-        'model_belief_by_trial.h5'))
+    old_store_pth = path(results_path).dirname().joinpath(
+        'model_belief_by_trial.h5').abspath()
+    old_store = pd.HDFStore(old_store_pth, mode='r')
     store = pd.HDFStore(results_path, mode='w')
 
-    # L2 regularization is equivalent to using a laplace prior
-    laplace_prior = make_prior(log_laplace, mu=1, b=1)
+    rc = Client()
+    lview = rc.load_balanced_view()
+    results = []
 
     for key in old_store.keys():
-        print key
         if key.split('/')[-1] == 'param_ref':
             store.append(key, old_store[key])
             continue
 
-        llh = old_store[key]\
-            .set_index(['stimulus', 'model'], append=True)['logp']\
-            .unstack('hypothesis')
+        result = lview.apply(task, [key, responses, old_store_pth, path.getcwd()])
+        results.append(result)
 
-        # compute the log likelihood ratio between the two hypotheses
-        llr = llh[-1] - llh[1]
+    while len(results) > 0:
+        result = results.pop(0)
+        if not result.ready():
+            results.append(result)
+            continue
 
-        model = llr\
-            .reset_index()\
-            .rename(columns={0: 'llr'})\
-            .set_index(['version', 'pid', 'trial', 'stimulus'])
-        model['responses'] = responses['mass? response'].copy()
-        model['kappa0'] = responses['kappa0'].copy()
-        model = model\
-            .reset_index()\
-            .set_index(['model', 'version', 'pid', 'trial', 'stimulus'])\
-            .sortlevel()
-
-        # use L2 logistic regression to fit parameters individually to
-        # each participant in version G
-        res_G = model\
-            .groupby(level='version')\
-            .get_group('G')\
-            .groupby(level=['model', 'pid'])\
-            .apply(fit_responses, laplace_prior)
-        params_G = res_G\
-            .reset_index()[['model', 'version', 'kappa0', 'pid', 'B']]\
-            .drop_duplicates()\
-            .set_index(['model', 'version', 'kappa0', 'pid'])['B']
-
-        # use the parameters fit to participants in version G as a prior
-        # over parameters, and then fit parameters to each participant
-        empirical_priors = params_G.groupby(level='model').apply(
-            lambda x: make_prior(util.kde, np.asarray(x), 0.1))
-        res_ind = model\
-            .drop('G', level='version')\
-            .groupby(level=['version', 'model', 'pid'])\
-            .apply(lambda x: fit_responses(x, empirical_priors[x.name[1]]))
-        params_ind = res_ind\
-            .reset_index()[['model', 'version', 'kappa0', 'pid', 'B']]\
-            .drop_duplicates()\
-            .set_index(['model', 'version', 'kappa0', 'pid'])['B']
-
-        results = res_G.reset_index()
-        results['version'] = 'G'
-        results = pd\
-            .concat([results, res_ind.reset_index()])\
-            .dropna()\
-            .set_index(['model', 'version', 'kappa0', 'pid', 'trial', 'stimulus'])\
-            .sortlevel()\
-            .drop('B', axis=1)
-
-        params = pd.concat([params_G, params_ind]).sortlevel()
-
-        store.append("{}/belief".format(key), results)
+        result.display_outputs()
+        key, belief, params = result.get()
+        store.append("{}/belief".format(key), belief)
         store.append("{}/params".format(key), params)
 
     store.close()
