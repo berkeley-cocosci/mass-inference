06443357d74c023403bea6cba71e002715f34017
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-10-13 10:12:06 -0700

    Save models to separate arrays

--- analysis/analyses/model_belief_by_trial.py
+++ analysis/analyses/model_belief_by_trial.py
@@ -10,27 +10,37 @@ def run(results_path, seed):
     np.random.seed(seed)
 
     old_store = pd.HDFStore('results/model_likelihood_by_trial.h5')
-    store = pd.HDFStore(results_path)
+    store = pd.HDFStore(results_path, mode='w')
 
     for key in old_store.keys():
+        print key
         if key.split('/')[-1] == 'param_ref':
             store.append(key, old_store[key])
             continue
 
-        print key
         llh = old_store[key]\
-            .set_index(['stimulus', 'kappa0'], append=True)['logp']\
+            .set_index(['stimulus'], append=True)['logp']\
             .unstack('hypothesis')
 
         static = util.normalize(np.asarray(llh), axis=1)[1]
         static_df = llh.copy()
         static_df[:] = static
-        store.append('{}/static'.format(key), llh)
+        static_df = static_df\
+            .stack()\
+            .reset_index('stimulus')\
+            .rename(columns={0: 'logp'})
+        static_df['model'] = 'static'
+        store.append("{}/static".format(key), static_df)
 
         chance = util.normalize(np.zeros_like(llh), axis=1)[1]
         chance_df = llh.copy()
         chance_df[:] = chance
-        store.append('{}/chance'.format(key), chance_df)
+        chance_df = chance_df\
+            .stack()\
+            .reset_index('stimulus')\
+            .rename(columns={0: 'logp'})
+        chance_df['model'] = 'chance'
+        store.append("{}/chance".format(key), chance_df)
 
         llhcum = llh\
             .reset_index('stimulus', drop=True)\
@@ -43,8 +53,14 @@ def run(results_path, seed):
         learning = util.normalize(np.asarray(llhcum), axis=1)[1]
         learning_df = llh.copy()
         learning_df[:] = learning
-        store.append('{}/learning'.format(key), learning_df)
+        learning_df = learning_df\
+            .stack()\
+            .reset_index('stimulus')\
+            .rename(columns={0: 'logp'})
+        learning_df['model'] = 'learning'
+        store.append("{}/learning".format(key), learning_df)
 
+    store.close()
 
 if __name__ == "__main__":
     util.run_analysis(run, sys.argv[1])
