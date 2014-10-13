027db107969657e6a965897c4b0d165ede8ab19b
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-10-13 10:09:29 -0700

    Small bugfixes in model_likelihood_by_trial.py

--- analysis/analyses/model_likelihood_by_trial.py
+++ analysis/analyses/model_likelihood_by_trial.py
@@ -7,7 +7,7 @@ import numpy as np
 from path import path
 
 
-def make_model_df(arr, trials, hyps, kappa0, pid):
+def make_model_df(arr, trials, hyps, kappa0, pid, version):
     new_df = pd.DataFrame(arr, index=trials['stimulus'], columns=hyps)
     new_df.index.name = 'stimulus'
     new_df.columns.name = 'hypothesis'
@@ -17,7 +17,9 @@ def make_model_df(arr, trials, hyps, kappa0, pid):
     new_df = new_df.reset_index()
     new_df['kappa0'] = kappa0
     new_df['pid'] = pid
-    new_df = new_df.set_index(['pid', 'trial', 'hypothesis'])
+    new_df['version'] = version
+    new_df = new_df.set_index([
+        'version', 'kappa0', 'pid', 'trial', 'hypothesis'])
     return new_df
 
 
@@ -26,7 +28,7 @@ def run(results_path, seed):
     data = util.load_human()
 
     old_store = pd.HDFStore('results/model_likelihood.h5')
-    store = pd.HDFStore(results_path)
+    store = pd.HDFStore(results_path, mode='w')
 
     pth = path(results_path).dirname().joinpath("trial_order.csv")
     trials = pd.read_csv(pth)\
@@ -53,21 +55,26 @@ def run(results_path, seed):
             continue
 
         llh = old_store[key]\
+            .reset_index('kappa0')
+        llh['kappa0'] = llh['kappa0'].apply(str)
+        llh = llh\
+            .set_index('kappa0', append=True)\
             .reorder_levels(['kappa0', 'stimulus'])
         hyps = llh.columns
 
         for (version, kappa0), pids in participants.groupby(level=['version', 'kappa0']):
             print key, version, kappa0
 
-            df = np.asarray(llh.ix[kappa0])
+            df = np.asarray(llh.ix[str(kappa0)])
 
             for pid in pids:
                 order, i = orders[pid]
                 dfi = df[i]
 
-                model = make_model_df(dfi, order, hyps, kappa0, pid)
-                store.append('{}/{}'.format(key, version), model)
+                model = make_model_df(dfi, order, hyps, kappa0, pid, version)
+                store.append(key, model)
 
+    store.close()
 
 if __name__ == "__main__":
     util.run_analysis(run, sys.argv[1])
