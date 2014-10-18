fe58b9737fa30fa6d386333bc84ba7fbce32e871
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-10-17 19:24:43 -0700

    Update model params plot

--- analysis/plots/model_params.py
+++ analysis/plots/model_params.py
@@ -10,31 +10,45 @@ import numpy as np
 
 def plot(results_path, fig_paths):
     data = pd.read_csv('results/model_params.csv')
-    fig, axes = plt.subplots(1, 3, sharey=True)
-    order = ['G', 'H', 'I']
-    colors = ['r', 'g', 'b']
+    fig, axes = plt.subplots(2, 3, sharey=True, sharex=True)
+    version_order = ['H', 'G', 'I']
+    model_order = ['static', 'learning']
+    colors = {
+        'learning': 'r',
+        'static': 'b'
+    }
+    versions = {
+        'G': 'Experiment 2a',
+        'H': 'Experiment 1',
+        'I': 'Experiment 2b'
+    }
 
-    for version, params in data.groupby('version'):
-        i = order.index(version)
-        ax = axes[i]
+    for (version, model), params in data.groupby(['version', 'model']):
+        if model not in model_order:
+            continue
 
-        for i, (m, df) in enumerate(params.groupby('model')):
-            B = np.asarray(df['B'].dropna())
-            ax.hist(
-                B, label=m, bins=38, alpha=0.5,
-                range=[-0.5, 2.5], normed=True, color=colors[i])
+        j = version_order.index(version)
+        i = model_order.index(model)
+        ax = axes[i, j]
 
-            x = np.linspace(-0.5, 2.5, 100)
-            y = util.kde(x[:, None], B, 0.1)
-            ax.plot(x, np.exp(y), color=colors[i])
+        B = np.asarray(params['B'].dropna())
+        ax.hist(
+            B, label=model, bins=38,
+            range=[-0.5, 2.5], normed=True,
+            color=colors[model])
 
-        ax.set_title(version)
+        # x = np.linspace(-0.5, 2.5, 100)
+        # y = util.kde(x[:, None], B, 0.1)
+        # ax.plot(x, np.exp(y), color=colors[m])
+
+        if i == 0:
+            ax.set_title(versions[version])
         util.clear_right(ax)
         util.clear_top(ax)
         util.outward_ticks(ax)
         ax.set_axis_bgcolor('0.9')
 
-    axes[0].legend(loc='best', fontsize=11, frameon=False)
+    axes[0, 0].legend(loc='best', fontsize=11, frameon=False)
 
     fig.set_figwidth(10)
     fig.set_figheight(3)
