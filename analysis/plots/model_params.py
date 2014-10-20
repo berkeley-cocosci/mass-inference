006cabed4b9469f76257deeb2f5460adb36072fe
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-10-20 10:57:41 -0700

    Update model params figure

--- analysis/plots/model_params.py
+++ analysis/plots/model_params.py
@@ -4,13 +4,14 @@ import util
 
 import sys
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import pandas as pd
 import numpy as np
 
 
 def plot(results_path, fig_paths):
     data = pd.read_csv('results/model_params.csv')
-    fig, axes = plt.subplots(2, 3, sharey=True, sharex=True)
+    fig, axes = plt.subplots(2, 3, sharex=True)
     version_order = ['H', 'G', 'I']
     model_order = ['static', 'learning']
     colors = {
@@ -34,12 +35,8 @@ def plot(results_path, fig_paths):
         B = np.asarray(params['B'].dropna())
         ax.hist(
             B, label=model, bins=38,
-            range=[-0.5, 2.5], normed=True,
+            range=[-0.5, 2.5], normed=False,
             color=colors[model])
-
-        # x = np.linspace(-0.5, 2.5, 100)
-        # y = util.kde(x[:, None], B, 0.1)
-        # ax.plot(x, np.exp(y), color=colors[m])
 
         if i == 0:
             ax.set_title(versions[version])
@@ -48,10 +45,25 @@ def plot(results_path, fig_paths):
         util.outward_ticks(ax)
         ax.set_axis_bgcolor('0.9')
 
-    axes[0, 0].legend(loc='best', fontsize=11, frameon=False)
+        ax.set_yticks(np.linspace(0, len(B), 6))
+        yticks = ax.get_yticks()
+        if j == 0:
+            yticklabels = ["{:.1f}".format(tick / float(len(B))) for tick in yticks]
+            ax.set_yticklabels(yticklabels)
+            ax.set_ylabel("% participants")
+        else:
+            ax.set_yticklabels([])
 
-    fig.set_figwidth(10)
-    fig.set_figheight(3)
+        if i == 1:
+            ax.set_xlabel(r"Value of $\beta$")
+
+    learning = mpatches.Patch(color=colors['learning'], label='learning')
+    static = mpatches.Patch(color=colors['static'], label='static')
+    axes[1, 0].legend(
+        handles=[static, learning], loc='best', fontsize=12, frameon=False)
+
+    fig.set_figwidth(12)
+    fig.set_figheight(4)
     plt.draw()
     plt.tight_layout()
 
