c2b6fda2362a0921be59ed4e34f51bfde4229deb
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-11-30 15:22:16 -0800

    Update human_mass_accuracy latex analysis

---
+++ analysis/latex_analyses/human_mass_accuracy.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+__depends__ = ["human_mass_accuracy.csv"]
+
+import os
+import util
+import pandas as pd
+
+
+def run(dest, results_path):
+    results = pd\
+        .read_csv(os.path.join(results_path, "human_mass_accuracy.csv"))\
+        .set_index(['version', 'kappa0']) * 100
+
+    latex_percent = util.load_config()["latex"]["percent"]
+
+    replace = {
+        'H': 'One',
+        'G': 'Two',
+        'I': 'Three',
+        '-1.0': 'KappaLow',
+        '1.0': 'KappaHigh',
+        'all': ''
+    }
+
+    fh = open(dest, "w")
+
+    for (version, kappa0), accuracy in results.iterrows():
+        cmdname = "MassAccExp{}{}".format(
+            replace[version], replace[kappa0])
+        cmd = latex_percent.format(**accuracy)
+        fh.write(util.newcommand(cmdname, cmd))
+
+    fh.close()
+
+
+if __name__ == "__main__":
+    parser = util.default_argparser(locals())
+    args = parser.parse_args()
+    run(args.dest, args.results_path)
