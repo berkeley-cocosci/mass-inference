a83654afa73faff01b79c8f017ea0a0d8bb46cac
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-11-30 15:15:18 -0800

    Update mass accuracy analysis

---
+++ analysis/analyses/human_mass_accuracy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+Computes overall participant accuracy for each version of the experiment for
+each separate mass ratio, and for both mass ratios combined.
+"""
+
+__depends__ = ["human"]
+
+import util
+import pandas as pd
+import numpy as np
+
+
+def run(dest, data_path, seed):
+    np.random.seed(seed)
+    human = util.load_human(data_path)['C'].dropna(subset=['mass? response'])
+
+    results = []
+    for kappa in [-1.0, 1.0, 'all']:
+        if kappa == 'all':
+            correct = human
+        else:
+            correct = human.groupby('kappa0').get_group(kappa)
+
+        accuracy = correct\
+            .groupby('version')['mass? correct']\
+            .apply(util.beta)\
+            .unstack(-1)\
+            .reset_index()
+
+        accuracy['kappa0'] = kappa
+        results.append(accuracy)
+
+    results = pd.concat(results)\
+        .set_index(['version', 'kappa0'])\
+        .sortlevel()
+
+    results.to_csv(dest)
+
+if __name__ == "__main__":
+    parser = util.default_argparser(locals(), seed=True)
+    args = parser.parse_args()
+    run(args.dest, args.data_path, args.seed)
