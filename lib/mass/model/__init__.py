36fa8c3045c3f7546bfde30283af6ec6e9f2dead
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-12-01 00:57:38 -0800

    Remove old files

--- lib/mass/model/__init__.py
+++
@@ -1,5 +0,0 @@
-from ipe import IPE
-from empirical_ipe import EmpiricalIPE
-from feedback import Feedback, NoFeedback
-
-__all__ = ['IPE', 'EmpiricalIPE', 'Feedback', 'NoFeedback']
--- lib/mass/model/util.py
+++
@@ -1,9 +0,0 @@
-class LazyProperty(object):
-    def __init__(self, func):
-        self._func = func
-        self.__name__ = func.__name__
-
-    def __get__(self, obj, klass):
-        result = self._func(obj)
-        obj.__dict__[self.__name__] = result
-        return result
