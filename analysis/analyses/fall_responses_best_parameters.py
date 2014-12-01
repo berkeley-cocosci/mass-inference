d80e3dbc4fe69b750dd9b09a2337be989ce14504
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-11-30 18:10:33 -0800

    Update best parameters analysis

--- analysis/analyses/fall_responses_best_parameters.py
+++ analysis/analyses/fall_responses_best_parameters.py
@@ -1,59 +1,72 @@
 #!/usr/bin/env python
 
 """
-Computes the squared error between model and human judgments on "will it fall?"
-for each stimulus for all the different parameter combinations of sigma/phi.
-Produces a csv file with the following columns:
+Computes the pearson correlation between model and human judgments on "will it
+fall?" for each stimulus for all the different parameter combinations of
+sigma/phi. Produces a csv file with the following columns:
 
-    stimulus (string)
-        stimulus name
-    kappa (float)
-        log mass ratio
     sigma (float)
         perceptual uncertainty
     phi (float)
         force uncertainty
-    sqerr (float)
-        squared error between people and model
+    pearsonr (float)
+        pearson correlation between model and people
 
 """
 
-__depends__ = ["human_fall_responses.csv", "model_fall_responses.csv"]
+__depends__ = ["human_fall_responses.csv", "model_fall_responses.h5"]
 
+import os
 import util
 import pandas as pd
-import numpy as np
-from path import path
+import scipy.stats
 
 
-def run(results_path):
-    human = pd.read_csv(path(results_path).dirname().joinpath(
-        "fall_responses.csv"))
+def run(dest, results_path):
+    # load in the human data
+    human = pd\
+        .read_csv(os.path.join(results_path, "human_fall_responses.csv"))\
+        .groupby(['version', 'block'])\
+        .get_group(('GH', 'B'))\
+        .pivot('stimulus', 'kappa0', 'median')\
+        .stack()
+    kappas = human.index.get_level_values('kappa0').unique()
 
-    human = human\
-        .set_index(['version', 'block', 'species', 'stimulus', 'kappa0'])\
-        .ix[('GH', 'B', 'human')]['median']\
-        .sortlevel()
+    # load in the table of parameter mappings
+    query = util.get_query()
+    store = pd.HDFStore(
+        os.path.join(results_path, "model_fall_responses.h5"), 'r')
+    params = store["/{}/param_ref".format(query)]
 
-    ipe = util.load_model()[0]['B']
-    model = ipe.P_fall_mean_all[[-1.0, 1.0]].stack()
+    # empty list to put the correlations
+    corrs = []
 
-    results = []
-    for (sigma, phi), model_df in model.groupby(level=['sigma', 'phi']):
-        x = model_df.reset_index(['sigma', 'phi'], drop=True)
-        err = (x - human) ** 2
-        err.index = model_df.index
-        results.append(err)
+    # load in the model data for each parameter combination, one by one
+    for key in store.root._f_getChild("/{}".format(query))._v_children:
+        if key == 'param_ref':
+            continue
 
-    results = pd\
-        .concat(results)\
-        .reset_index(['sigma', 'phi'])\
-        .rename(columns={0: 'sqerr'})
+        data = store["{}/{}".format(query, key)]
+        sigma = params.ix[key]['sigma']
+        phi = params.ix[key]['phi']
 
-    results.to_csv(results_path)
+        model = data\
+            .groupby('block')\
+            .get_group('B')\
+            .pivot('stimulus', 'kappa0', 'median')[kappas]\
+            .stack()
+
+        corrs.append(dict(
+            sigma=sigma,
+            phi=phi,
+            pearsonr=scipy.stats.pearsonr(model, human)[0]))
+
+    results = pd.DataFrame(corrs).set_index(['sigma', 'phi']).sortlevel()
+    results.to_csv(dest)
+    store.close()
 
 
 if __name__ == "__main__":
     parser = util.default_argparser(locals())
     args = parser.parse_args()
-    run(args.results_path)
+    run(args.dest, args.results_path)
