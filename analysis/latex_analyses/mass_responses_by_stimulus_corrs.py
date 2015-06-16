e966ab42993ca34956459298aa91168201be1170
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-06-16 15:11:47 -0700

    Report both cf and non-cf results

--- analysis/latex_analyses/mass_responses_by_stimulus_corrs.py
+++ analysis/latex_analyses/mass_responses_by_stimulus_corrs.py
@@ -12,22 +12,26 @@ import util
 import pandas as pd
 
 
-def run(dest, results_path, counterfactual):
+def run(dest, results_path):
     results = pd.read_csv(
         os.path.join(results_path, "mass_responses_by_stimulus_corrs.csv"))
 
     results = results\
-        .groupby(['version', 'model', 'counterfactual', 'fitted'])\
-        .get_group(('H', 'static', counterfactual, False))\
-        .set_index('likelihood')
+        .groupby(['version', 'model', 'fitted'])\
+        .get_group(('H', 'static', False))\
+        .set_index(['likelihood', 'counterfactual'])
 
     latex_pearson = util.load_config()["latex"]["pearson"]
 
     fh = open(dest, "w")
 
-    for lh, corrs in results.iterrows():
-        cmdname = "ExpOneMassRespStimCorr{}".format(
-            lh.replace('_', '').capitalize())
+    for (lh, cf), corrs in results.iterrows():
+        if cf:
+            suffix = "{}CF".format(lh.replace('_', '').capitalize())
+        else:
+            suffix = "{}NoCF".format(lh.replace('_', '').capitalize())
+
+        cmdname = "ExpOneMassRespStimCorr{}".format(suffix)
         cmd = latex_pearson.format(**corrs)
         fh.write(util.newcommand(cmdname, cmd))
 
@@ -35,21 +39,6 @@ def run(dest, results_path, counterfactual):
 
 
 if __name__ == "__main__":
-    config = util.load_config()
     parser = util.default_argparser(locals())
-    if config['analysis']['counterfactual']:
-        parser.add_argument(
-            '--no-counterfactual',
-            action='store_false',
-            dest='counterfactual',
-            default=True,
-            help="don't plot the counterfactual likelihoods")
-    else:
-        parser.add_argument(
-            '--counterfactual',
-            action='store_true',
-            dest='counterfactual',
-            default=False,
-            help='plot the counterfactual likelihoods')
     args = parser.parse_args()
-    run(args.to, args.results_path, args.counterfactual)
+    run(args.to, args.results_path)
--- analysis/latex_analyses/precision_recall.py
+++ analysis/latex_analyses/precision_recall.py
@@ -12,53 +12,43 @@ import util
 import pandas as pd
 
 
-def run(dest, results_path, counterfactual):
+def run(dest, results_path):
     results = pd.read_csv(
         os.path.join(results_path, "precision_recall.csv"))
 
     results = results\
-        .groupby(['version', 'model', 'counterfactual', 'fitted'])\
-        .get_group(('H', 'static', counterfactual, False))\
-        .set_index('likelihood')
+        .groupby(['version', 'model', 'fitted'])\
+        .get_group(('H', 'static', False))\
+        .set_index(['likelihood', 'counterfactual'])
 
     fh = open(dest, "w")
 
-    for lh, res in results.iterrows():
-        cmdname = "ExpOneFScore{}".format(lh.replace('_', '').capitalize())
+    for (lh, cf), res in results.iterrows():
+        if cf:
+            suffix = "{}CF".format(lh.replace('_', '').capitalize())
+        else:
+            suffix = "{}NoCF".format(lh.replace('_', '').capitalize())
+
+        cmdname = "ExpOneFScore{}".format(suffix)
         cmd = "F_1={F1:.2f}".format(**res)
         fh.write(util.newcommand(cmdname, cmd))
 
-        cmdname = "ExpOnePrecision{}".format(lh.replace('_', '').capitalize())
+        cmdname = "ExpOnePrecision{}".format(suffix)
         cmd = "{precision:.2f}".format(**res)
         fh.write(util.newcommand(cmdname, cmd))
 
-        cmdname = "ExpOneRecall{}".format(lh.replace('_', '').capitalize())
+        cmdname = "ExpOneRecall{}".format(suffix)
         cmd = "{recall:.2f}".format(**res)
         fh.write(util.newcommand(cmdname, cmd))
 
-        cmdname = "ExpOneAccuracy{}".format(lh.replace('_', '').capitalize())
-        cmd = "{accuracy:.2f}".format(**res)
+        cmdname = "ExpOneAccuracy{}".format(suffix)
+        cmd = r"{:.1f}\%".format(res['accuracy'] * 100)
         fh.write(util.newcommand(cmdname, cmd))
 
     fh.close()
 
 
 if __name__ == "__main__":
-    config = util.load_config()
     parser = util.default_argparser(locals())
-    if config['analysis']['counterfactual']:
-        parser.add_argument(
-            '--no-counterfactual',
-            action='store_false',
-            dest='counterfactual',
-            default=True,
-            help="don't plot the counterfactual likelihoods")
-    else:
-        parser.add_argument(
-            '--counterfactual',
-            action='store_true',
-            dest='counterfactual',
-            default=False,
-            help='plot the counterfactual likelihoods')
     args = parser.parse_args()
-    run(args.to, args.results_path, args.counterfactual)
+    run(args.to, args.results_path)
