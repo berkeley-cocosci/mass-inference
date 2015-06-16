a709c4be5353001976b65c898e7da2cb4a48124e
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-06-15 17:20:43 -0700

    Revert changes to calculating corrs

--- analysis/analyses/fall_response_corrs.py
+++ analysis/analyses/fall_response_corrs.py
@@ -26,70 +26,46 @@ Outputs a csv file with the following columns:
 
 """
 
-__depends__ = ["human_fall_responses_raw.csv", "single_model_fall_responses_raw.csv"]
+__depends__ = ["human_fall_responses.csv", "single_model_fall_responses.csv"]
 __random__ = True
 
 import pandas as pd
 import numpy as np
 import util
 import os
-import scipy.stats
-
-
-def bootsamps(x, func=np.mean, n=10000):
-    x_ = x.dropna()
-    ix = np.random.randint(0, x_.size, (n, x_.size))
-    samps = func(np.asarray(x_)[ix], axis=1)
-    samps = pd.Series(samps, name=x.name)
-    samps.index.name = 'sample'
-    return samps
-
-
-def corr(x, y):
-    df = pd.DataFrame(dict(x=x, y=y))
-    corrs = df.groupby(level='sample').apply(lambda x: scipy.stats.pearsonr(x['x'], x['y'])[0])
-    r = pd.Series(np.percentile(corrs, [2.5, 50, 97.5]), index=['lower', 'median', 'upper'])
-    return r
 
 
 def run(dest, results_path, seed, version):
     np.random.seed(seed)
 
-    human = pd.read_csv(os.path.join(results_path, "human_fall_responses_raw.csv"))
+    human = pd.read_csv(os.path.join(results_path, "human_fall_responses.csv"))
     human = human.groupby('version').get_group(version)
 
-    model = pd.read_csv(os.path.join(results_path, "single_model_fall_responses_raw.csv"))
+    model = pd.read_csv(os.path.join(results_path, "single_model_fall_responses.csv"))
 
     results = {}
     for (query, block), df in model.groupby(['query', 'block']):
-        print query, block
-
         h = human\
             .groupby('block')\
             .get_group(block)\
-            .groupby(['stimulus', 'kappa0'])['fall? response']\
-            .apply(bootsamps)\
-            .unstack('kappa0')
+            .pivot('stimulus', 'kappa0', 'median')
 
-        m = df\
-            .groupby(['stimulus', 'kappa0'])['response']\
-            .apply(bootsamps)\
-            .unstack('kappa0')
+        m = df.pivot('stimulus', 'kappa0', 'median')
 
         # human vs human
         x = h[-1.0]
         y = h[1.0]
-        results[(query, block, 'Human', 'Human')] = corr(x, y)
+        results[(query, block, 'Human', 'Human')] = util.bootcorr(x, y)
 
         # mass-sensitive ipe vs human
         x = pd.concat([m[-1.0], m[1.0]])
         y = pd.concat([h[-1.0], h[1.0]])
-        results[(query, block, 'ModelS', 'Human')] = corr(x, y)
+        results[(query, block, 'ModelS', 'Human')] = util.bootcorr(x, y)
 
         # mass-insensitive ipe vs human
         x = pd.concat([m[0.0], m[0.0]])
         y = pd.concat([h[-1.0], h[1.0]])
-        results[(query, block, 'ModelIS', 'Human')] = corr(x, y)
+        results[(query, block, 'ModelIS', 'Human')] = util.bootcorr(x, y)
 
     results = pd.DataFrame.from_dict(results).T
     results.index = pd.MultiIndex.from_tuples(
