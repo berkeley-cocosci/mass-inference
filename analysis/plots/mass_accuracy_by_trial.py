bb7e551a447e92c1aaa9f7ac3bf40dbafd1ae52c
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-07-29 18:59:15 -0700

    Clean up mass accuracy plot

--- analysis/plots/mass_accuracy_by_trial.py
+++ analysis/plots/mass_accuracy_by_trial.py
@@ -13,15 +13,17 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 
 
-def timeseries(ax, x, color, label=None):
-    ax.fill_between(
-        x['trial'], x['lower'], x['upper'],
-        color=color, alpha=0.3)
+def timeseries(ax, x, color, label=None, marker='o', markersize=6, fill=True):
+    if fill:
+        ax.fill_between(
+            x['trial'], x['lower'], x['upper'],
+            color=color, alpha=0.3)
     ax.plot(
         x['trial'], x['median'], 
-        lw=2, marker='o', ms=6, color=color, label=label)
+        lw=2, marker=marker, ms=markersize, color=color, label=label)
 
 
 def plot(dest, results_path):
@@ -36,35 +38,40 @@ def plot(dest, results_path):
     # load in colors
     plot_config = util.load_config()["plots"]
     darkgrey = plot_config["darkgrey"]
-    colors = plot_config["colorcircle"][::-1]
+    colors = np.ones((5, 3)) * np.linspace(0.1, 0.6, 5)[:, None]
+    markers = ['s', 'd', '*', '^', 'o']
+    markersizes = [6, 7, 10, 7, 6]
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True)
 
     # left subplot: experiment 2
     timeseries(ax1, responses.ix['G'], darkgrey)
-    ax1.set_title('Experiment 1\n')
+    ax1.set_title('Experiment 2\n')
     ax1.set_xlabel('Trial')
-    ax1.set_ylabel('Fraction correct')
+    ax1.set_ylabel('Proportion correct')
     ax1.set_xticks([1, 2, 3, 4, 6, 9, 14, 20])
-    ax1.set_xlim([1, 20])
+    ax1.set_xlim([1, 20.5])
     ax1.set_ylim(0.5, 1)
 
     # middle subplot: experiment 3 (between subjects)
     timeseries(ax2, responses.ix[('I', -1)], darkgrey)
-    ax2.set_title('Experiment 2\n(between subjects)')
+    ax2.set_title('Experiment 3\n(between subjects)')
     ax2.set_xlabel('Trial')
     ax2.set_xticks([1, 2, 3, 5, 10])
-    ax2.set_xlim([1, 10])
+    ax2.set_xlim([1, 10.25])
 
     # right subplot: experiment 3 (within subjects)
-    for i in range(1, 6):
+    for i in range(5, 0, -1):
         timeseries(
-            ax3, responses.ix[('I', i)], colors[i - 1], 
-            label="{} trials".format(i))
+            ax3, responses.ix[('I', i)], colors[i - 1],
+            label="{} trials".format(i),
+            marker=markers[i - 1],
+            markersize=markersizes[i - 1],
+            fill=False)
     ax3.set_title('Experiment 3\n(within subjects)')
     ax3.set_xlabel('Trial')
     ax3.set_xticks([1, 2, 3, 5, 10])
-    ax3.set_xlim([1, 10])
+    ax3.set_xlim([1, 10.25])
 
     # draw the legend
     ax3.legend(loc='lower center', fontsize=10, ncol=2, frameon=False)
